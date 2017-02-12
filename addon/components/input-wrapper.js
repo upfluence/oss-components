8ce83c976d2be91a5a1ae5162b3a3aaa1245aa73
Philippe Ndiaye <phndiaye@gmail.com>
2017-02-12 01:41:02 +0100

    added support for inputs offering help

--- addon/components/input-wrapper.js
+++ addon/components/input-wrapper.js
@@ -2,14 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   error: null,
+  help: null,
 
   tagName: 'div',
   classNames: ['form-group', 'upf-input-container'],
-  classNameBindings: ['errorful'],
+  classNameBindings: ['errorful', 'hasHelp'],
 
   errorful: Ember.computed('error', function() {
     if (this.get('error') != null) {
       return 'upf-input-container--errorful';
+    }
+  }),
+
+  hasHelp: Ember.computed('help', function() {
+    if (this.get('help') != null) {
+      return 'upf-input-container--has-help';
     }
   }),
 });
