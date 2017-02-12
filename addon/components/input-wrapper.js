372072f80239716cc3ca49e6bb4b7ad19ffac8f6
Philippe Ndiaye <phndiaye@gmail.com>
2017-02-12 01:35:08 +0100

    add support for form groups with error feedbacks

--- addon/components/input-wrapper.js
+++ addon/components/input-wrapper.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  error: null,
+
   tagName: 'div',
   classNames: ['form-group', 'upf-input-container'],
+  classNameBindings: ['errorful'],
+
+  errorful: Ember.computed('error', function() {
+    if (this.get('error') != null) {
+      return 'upf-input-container--errorful';
+    }
+  }),
 });
