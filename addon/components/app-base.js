6b70929906d68647ce2860a6fdce8b5ec3fa1a80
Philippe Ndiaye <phndiaye@gmail.com>
2017-02-15 00:16:42 +0100

    give the playground examples more space

--- addon/components/app-base.js
+++ addon/components/app-base.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   contentContainerClass: Ember.computed('headerStyle', function() {
     if (this.get('headerStyle') === 'sidebar') {
-      return 'col-xs-11';
+      return 'col-xs-11 app-main--along-sidebar';
     } else {
-      return 'col-xs-12';
+      return 'col-xs-12 app-main--below-topbar';
     }
   }),
 });
