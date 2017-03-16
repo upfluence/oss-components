48aa814664018098e962ab08689d445d64321c9d
Philippe Ndiaye <phndiaye@gmail.com>
2017-03-16 17:25:40 +0100

    updated the standard stat component to make use of the new design

--- addon/components/standard-stat.js
+++ addon/components/standard-stat.js
@@ -1,7 +1,39 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const {
+  Component,
+  computed
+} = Ember;
 
 export default Component.extend({
   classNames: ['upf-stat'],
+
+  defaultRadialSize: 'upf-progress-radial-lg',
+  defaultRadialClass: 'upf-progress-radial--none',
+
+  type: null,
+  size: null,
+  iconClass: null,
+  number: 1,
+
+  radialClass: computed('type', function() {
+    if (this.get('type')) {
+      return this.get('type');
+    }
+
+    return this.get('defaultRadialClass');
+  }),
+
+
+  radialSize: computed('size', function() {
+    if (this.get('size')) {
+      return this.get('size');
+    }
+
+    return this.get('defaultRadialSize');
+  }),
+
+  progressValueClass: computed('number', function() {
+    return `progress-${this.get('number')}`;
+  })
 })
