136f314b19099ae9d780d3ac85b6273056b56ede
Philippe Ndiaye <phndiaye@gmail.com>
2017-03-18 13:57:38 +0100

    glob the oss asset dependencies import instead of importing them one at a time by hand

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* jshint node: true */
 'use strict';
 
+var mergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
   name: 'oss-components',
 
@@ -15,28 +18,29 @@ module.exports = {
     app.import('bower_components/summernote/dist/font/summernote.woff', { destDir: 'assets/font' });
     app.import('bower_components/money-formatter/dist/money-formatter.min.js');
     app.import('bower_components/countdown.js/lib/countdown.js');
-
-    //
-    // Upfluence OSS framework dependencies
-    //
-    app.import('bower_components/upfluence-oss/fonts/GT-Eesti-Display-Medium.ttf', { destDir: 'assets/fonts' });
-    app.import('bower_components/upfluence-oss/fonts/GT-Eesti-Display-Medium.eot', { destDir: 'assets/fonts' });
-    app.import('bower_components/upfluence-oss/fonts/GT-Eesti-Display-Medium.woff', { destDir: 'assets/fonts' });
-    app.import('bower_components/upfluence-oss/images/upfluence-fire-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/air-1-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/earth-1-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/earth-3-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/fire-2-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/spirit-2-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/spirit-3-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/water-1-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/water-2-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/images/water-3-white.png', { destDir: 'assets/images' });
-    app.import('bower_components/upfluence-oss/upf-icons/reply.svg', { destDir: 'assets/upf-icons' });
-    app.import('bower_components/upfluence-oss/upf-icons/envelope.svg', { destDir: 'assets/upf-icons' });
-    app.import('bower_components/upfluence-oss/upf-icons/envelope-open.svg', { destDir: 'assets/upf-icons' });
-    app.import('bower_components/upfluence-oss/upf-icons/paper-plane.svg', { destDir: 'assets/upf-icons' });
-    app.import('bower_components/upfluence-oss/upf-icons/clock.svg', { destDir: 'assets/upf-icons' });
-    app.import('bower_components/upfluence-oss/upf-icons/exclamation-mark.svg', { destDir: 'assets/upf-icons' });
   },
+
+  treeForPublic: function() {
+    const publicTree = this._super.treeForPublic.apply(this, arguments);
+    var trees = [];
+
+    if (publicTree) {
+      trees.push(publicTree);
+    }
+
+    const publicAssets = ['images', 'fonts', 'upf-icons'];
+    publicAssets.forEach( (assetType) => {
+      trees.push(
+        new Funnel(
+          `${this.app.bowerDirectory}/upfluence-oss/${assetType}/`,
+          {
+            srcDir: '/',
+            destDir: `assets/${assetType}`,
+          }
+        )
+      );
+    })
+
+    return mergeTrees(trees);
+  }
 };
