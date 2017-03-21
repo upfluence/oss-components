e4a46ccfe12b6fe58c3db48169732f247ff02399
Philippe Ndiaye <phndiaye@gmail.com>
2017-03-22 00:53:10 +0100

    looks like we will finally need the maxValue and percents computation

--- addon/components/standard-stat.js
+++ addon/components/standard-stat.js
@@ -14,7 +14,11 @@ export default Component.extend({
   iconClass: null,
   number: 1,
 
+  numberAsPercentOfMaxValue: computed('number', 'maxValue', function() {
+    return Math.floor((100 * this.get('number')) / this.get('maxValue'));
+  }),
+
   progressValueClass: computed('number', function() {
-    return `upf-progress-${this.get('number')}`;
+    return `upf-progress-${this.get('numberAsPercentOfMaxValue')}`;
   })
 })
