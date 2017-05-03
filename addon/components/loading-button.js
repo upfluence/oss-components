482a8dfaa33d86c92b9ce10bf697b3e6a52be93d
Philippe Ndiaye <phndiaye@gmail.com>
2017-05-03 11:23:21 +0200

    refactor a little the component code

--- addon/components/loading-button.js
+++ addon/components/loading-button.js
@@ -7,22 +7,27 @@ const {
 
 const LoadingButtonComponent = Component.extend({
   tagName: 'button',
+  classNameBindings: ['isLoading:js-btn--loading'],
+  attributeBindings: ['isLoading:disabled'],
 
   click(e) {
     e.preventDefault();
+
+    let originalContent = this.$().html();
+    let originalWidth = this.$().width();
 
     // Because `.send` method for sending actions does not return anything, we
     // pass it an `RSVP.defer` to be resolved in the remote action.
     let defer = RSVP.defer();
     defer.promise.then(() => {
-      this.$().text(this.get('originalContent'));
-    }, () => {
-
+      this.$().removeAttr('style');
+      this.set('isLoading', false);
+      this.$().html(originalContent);
     });
 
     Ember.run.debounce(this, function() {
-      this.$().width(this.$().width());
-      this.set('originalContent', this.$().html());
+      this.set('isLoading', true);
+      this.$().width(originalWidth);
       this.$().html("<i class='fa fa-circle-o-notch fa-spin'></i>");
 
       this.get('targetObject').send(
