8b5d50dcacaf5187490f4b011c9d894893505220
Philippe Ndiaye <phndiaye@gmail.com>
2017-07-07 17:13:32 +0200

    drop the header support, we are going full sidebar everywhere now

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 import HeaderStyleMixin from 'oss-components/mixins/header-style';
 
 export default Ember.Controller.extend(HeaderStyleMixin, {
-  headerStyle: 'topbar',
-
-  mainContentContainerClass: Ember.computed('headerStyle', function() {
-    if (this.get('headerStyle') === 'sidebar') {
-      return 'col-xs-11 app-main--along-sidebar';
-    } else {
-      return 'col-xs-12 app-main--below-topbar';
-    }
-  }),
-
   actions: {
     switchHeaderStyle: function() {
       var headerStyle = (this.get('headerStyle') === 'sidebar') ? 'topbar' : 'sidebar';
