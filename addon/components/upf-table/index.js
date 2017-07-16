66fe9ae718f7231188656f1bd3960adee4d04b29
Philippe Ndiaye <phndiaye@gmail.com>
2017-07-16 23:29:03 +0200

    added basic support for toggling a column visibility

--- addon/components/upf-table/index.js
+++ addon/components/upf-table/index.js
@@ -1,16 +1,24 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const {
+  Component,
+  computed
+} = Ember;
 
 export default Component.extend({
-  tagName: 'table',
-
   hasSelection: false,
   hasSortableColumns: true,
 
+  _columns: computed('columns', function() {
+    return this.get('columns').map((column) => {
+      column.visible = (column.visible !== false) ? true : column.visible;
+      return Ember.Object.create(column);
+    })
+  }),
+
   didInsertElement() {
     if (this.get('hasSortableColumns')) {
-      this.$().tablesorter({
+      this.$('table').tablesorter({
         cssAsc: 'upf-table__column--ascending',
         cssDesc: 'upf-table__column--descending',
         headers: {
