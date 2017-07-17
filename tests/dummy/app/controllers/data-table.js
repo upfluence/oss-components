3c8988e048dfc7b327f378fb65f0e817c7b0eee8
Philippe Ndiaye <phndiaye@gmail.com>
2017-07-17 16:15:45 +0200

    remove the old columns configuration

--- tests/dummy/app/controllers/data-table.js
+++ tests/dummy/app/controllers/data-table.js
@@ -4,69 +4,11 @@ export default Ember.Controller.extend({
   columns: [
     { title: 'Name', property: 'name' },
     { title: 'Media URL', property: 'media_url', visible: false },
-    { title: 'Email', property: 'email' }
-  ],
-
-  tableColumns: [
-    {
-      'disableFiltering': true,
-      'mayBeHidden': false,
-      'component': 'data-table/select-row',
-      'className': 'upf-table__row-selector'
-    },
-    {
-      'propertyName': 'name',
-      'title': 'Influencer',
-      'grouped_with': 'media_url,email',
-      'component': 'data-table/group-column',
-    },
-    {
-      'propertyName': 'supplier_interest',
-    },
-    {
-      'propertyName': 'selectability',
-    },
-    {
-      'propertyName': 'proposed_price',
-      'title': 'Negociated price',
-      'icon': 'fa fa-money',
-      'componentForSortCell': 'data-table/icon-header',
-      'component': 'data-table/money-column',
-    },
-    {
-      'propertyName': 'price',
-      'title': 'Price',
-      'icon': 'fa fa-handshake-o',
-      'componentForSortCell': 'data-table/icon-header',
-      'component': 'data-table/money-column',
-    },
-    {
-      'propertyName': 'visible_by_client',
-      'title': 'Visible by client',
-      'icon': 'fa fa-user-secret upf-icon-group__superior--visibility',
-      'componentForSortCell': 'data-table/icon-header',
-      'component': 'data-table/switchable-boolean-column'
-    },
-    {
-      'propertyName': 'client_approved_for_drafting',
-      'title': 'Client approved for drafting',
-      'icon': 'fa fa-user-secret upf-icon-group__superior--edit',
-      'componentForSortCell': 'data-table/icon-header',
-      'component': 'data-table/switchable-boolean-column'
-    },
-    {
-      'propertyName': 'admin_approved_for_drafting',
-      'title': 'Admin approved for drafting',
-      'icon': 'fa fa-user upf-icon-group__superior--edit',
-      'componentForSortCell': 'data-table/icon-header',
-      'component': 'data-table/switchable-boolean-column',
-    },
-    {
-      'mayBeHidden': false,
-      'title': 'Details',
-      'template': 'components/models-table/expand-row-cell',
-      'className': 'upf-align--center',
-    },
+    { title: 'Email', property: 'email' },
+    { title: 'selectability', property: 'selectability' },
+    { title: 'fff', property: 'client_approved_for_drafting' },
+    { title: 'popopo', property: 'proposed_price' },
+    { title: 'selected', property: 'isSelected' }
   ],
 
   actions: {
