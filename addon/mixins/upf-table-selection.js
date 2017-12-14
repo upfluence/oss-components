a61a5ef3cacbc68110efab18bfe06c061e03c332
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-14 16:02:03 +0100

    fix linting comments

--- addon/mixins/upf-table-selection.js
+++ addon/mixins/upf-table-selection.js
@@ -8,11 +8,11 @@ const {
 export default Mixin.create({
   selectedItems: computed('collection.@each.selected', function() {
     return this.get('collection').filter((item) => {
-        return item.get('selected');
+      return item.get('selected');
     });
   }),
 
   emptySelection: computed('selectedItems', function() {
     return this.get('selectedItems.length') === 0;
-  }),
+  })
 });
