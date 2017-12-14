45ea919218f81397f5b23da7f2187a4ca6f0142a
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-14 16:01:13 +0100

    a mixin to apply on contextual actions component to get selected stuffs and all

---
+++ addon/mixins/upf-table-selection.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+const {
+  Mixin,
+  computed
+} = Ember;
+
+export default Mixin.create({
+  selectedItems: computed('collection.@each.selected', function() {
+    return this.get('collection').filter((item) => {
+        return item.get('selected');
+    });
+  }),
+
+  emptySelection: computed('selectedItems', function() {
+    return this.get('selectedItems.length') === 0;
+  }),
+});
