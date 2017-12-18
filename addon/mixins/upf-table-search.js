a759c49ceb5b8974b09d0f8dcc26297e2e68efad
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-18 15:46:40 +0100

    fix imhotep comments

--- addon/mixins/upf-table-search.js
+++ addon/mixins/upf-table-search.js
@@ -17,8 +17,12 @@ export default Mixin.create({
       this,
       'filteredCollection',
       computed('searchQuery', this.get('searchCollection'), function() {
-        let searchWords = this.get('searchQuery').split(' ').map(x => x.toLowerCase());
-        let collection = get(this, this.get('searchCollection')).filterBy('isNew', false);
+        let searchWords = this.get('searchQuery').split(' ').map( (x) => {
+          x.toLowerCase()
+        });
+        let collection = get(this, this.get('searchCollection')).filterBy(
+          'isNew', false
+        );
 
         if (searchWords === []) {
           return collection;
