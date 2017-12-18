e8d11b1a219424465ba260a168312fccb67e79d5
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-18 15:45:40 +0100

    refresh table's content on collection's changes

--- addon/mixins/upf-table-search.js
+++ addon/mixins/upf-table-search.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 const {
   Mixin,
   computed,
+  defineProperty,
   get
 } = Ember;
 
@@ -10,27 +11,34 @@ export default Mixin.create({
   searchCollection: 'model',
   searchQuery: '',
 
-  filteredCollection: computed('searchQuery', function() {
-    let searchWords = this.get('searchQuery').split(' ').map(x => x.toLowerCase());
-    let collection = get(this, this.get('searchCollection'));
+  init() {
+    this._super();
+    defineProperty(
+      this,
+      'filteredCollection',
+      computed('searchQuery', this.get('searchCollection'), function() {
+        let searchWords = this.get('searchQuery').split(' ').map(x => x.toLowerCase());
+        let collection = get(this, this.get('searchCollection')).filterBy('isNew', false);
 
-    if (searchWords === []) {
-      return collection;
-    }
-
-    return collection.filter((item) => {
-      let itemName = item.get(this.get('searchAttribute')).toLowerCase();
-      let i;
-
-      for (i = 0; i < searchWords.length; i++) {
-        let searchMatch = itemName.includes(searchWords[i]);
-
-        if (searchMatch) {
-          return true;
+        if (searchWords === []) {
+          return collection;
         }
-      }
-    });
-  }),
+
+        return collection.filter((item) => {
+          let itemName = item.get(this.get('searchAttribute')).toLowerCase();
+          let i;
+
+          for (i = 0; i < searchWords.length; i++) {
+            let searchMatch = itemName.includes(searchWords[i]);
+
+            if (searchMatch) {
+              return true;
+            }
+          }
+        });
+      })
+    );
+  },
 
   actions: {
     performSearch(s) {
