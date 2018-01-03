2ad13613385459dd08afc7c581b0e3356b91bb4d
Philippe Ndiaye <phndiaye@gmail.com>
2018-01-03 10:44:53 +0100

    autofocus the search input when it opens

--- addon/components/expanding-search.js
+++ addon/components/expanding-search.js
@@ -20,6 +20,7 @@ export default Component.extend({
     expandOrErase() {
       if (!this.get('expandedSearch')) {
         this.set('expandedSearch', true);
+        this.$('.upf-input').focus();
       } else {
         this.set('searchQuery', '');
         this.set('expandedSearch', false);
