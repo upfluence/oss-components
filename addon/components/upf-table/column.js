783aee108ff051fd2d6f4b330dc2ddc871b445fc
Philippe Ndiaye <phndiaye@gmail.com>
2018-01-26 19:11:20 +0100

    fix inversed editable column setting

--- addon/components/upf-table/column.js
+++ addon/components/upf-table/column.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
   tagName: 'td',
   classNames: ['upf-datatable__column'],
   classNameBindings: [
-    'editable:upf-datatable__column--uneditable'
+    'uneditable:upf-datatable__column--uneditable'
   ],
   attributeBindings: ['valign'],
 
-  valign: 'middle'
+  valign: 'middle',
+  uneditable: computed.not('editable')
 });
