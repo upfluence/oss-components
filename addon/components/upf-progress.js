65a3bbb79fe4add01e06c67940a507795e355c95
Philippe Ndiaye <phndiaye@gmail.com>
2018-06-11 17:43:23 +0200

    fixes binding-style-attributes deprecation warnings

--- addon/components/upf-progress.js
+++ addon/components/upf-progress.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
   classNames: ['upf-progress'],
   classNameBindings: ['small:upf-progress--small'],
 
   value: null,
-  colorClass: null
+  colorClass: null,
+
+  maxWidthStyle: computed('value', function() {
+    return `max-width: ${this.get('value')}%`;
+  })
 });
