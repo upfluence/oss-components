4b206d86d9ef51b22c4d081c5a9f3f48e96de785
Philippe Ndiaye <phndiaye@gmail.com>
2018-12-24 13:24:37 +0100

    add doc introduction

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+/* global require module */
 /* jshint node: true */
 'use strict';
 
 const mergeTrees = require('broccoli-merge-trees');
 const Funnel = require('broccoli-funnel');
 
+/*
+---
+name: Get Started
+category: Introduction
+---
+
+OSS is a design system built to help Upfluence's developers and designers quickly create
+the best experience for our clients. At Upfluence, we love to create harmonious experiences
+for our users, solving challenging interactions once then applying the solutions consistently
+across our products — making sure users only have to learn them once.
+
+### Installing
+--------------
+
+There are two options for installing and implementing/using our components:
+**[Ember JS](https://www.emberjs.com/)** or **[LESS](http://lesscss.org)**.
+
+#### Ember JS
+
+The best way to use the components is through our Ember JS addon,
+ **[oss-components](https://github.com/upfluence/oss-components)**. You can install it
+via `Yarn` or `npm`:
+
+```
+yarn add upfluence-oss-components
+```
+
+#### LESS CSS
+
+If you don't want to use Ember JS or if you are working on a standalone project that only
+requires OSS's CSS, you can build it from our **[LESS repository](https://github.com/upfluence/oss)**
+*/
 module.exports = {
   name: 'oss-components',
 
