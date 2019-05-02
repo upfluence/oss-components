41227ab6b20838905f18b93dc4537a7fc8a02cc8
Philippe Ndiaye <phndiaye@gmail.com>
2019-05-02 11:32:58 +0200

    real placeholder image

--- addon/components/upf-image.js
+++ addon/components/upf-image.js
@@ -7,7 +7,7 @@ export default Component.extend({
 
   didInsertElement() {
     this.$().css({
-      'background-image': `url("${this.src}"), url("https://d25re8488fs7lz.cloudfront.net/image-server-assets/influencer-server/influencer/2233405.png")`
+      'background-image': `url("${this.src}"), url("assets/images/no-image.svg")`
     });
   }
 });
