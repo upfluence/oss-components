af80e838293e3ed584ac354efa2b58a40e53ac6f
Min S <mindysoh@gmail.com>
2019-06-13 09:29:39 -0400

    allow disabled option for radio button, while loading disabled buttons dont show if its checked

--- addon/components/radio-button.js
+++ addon/components/radio-button.js
@@ -1,18 +1,24 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 
 export default Component.extend({
   tagName: 'label',
   classNames: ['btn', 'upf-radio-btn'],
-  classNameBindings: ['isChecked:active'],
+  classNameBindings: ['isChecked:active', 'isDisabled:disabled'],
   attributeBindings: ['style'],
   style: computed('options', function() {
     return `width: ${100 / Object.keys(this.get('options')).length}%;`;
   }),
 
-  isChecked: computed('value', 'currentValue', function() {
-    return this.get('value') === this.get('currentValue');
+  isChecked: computed('value', 'currentValue', 'isDisabled', function() {
+    if (!this.isDisabled) {
+      return this.get('value') === this.get('currentValue');
+    }
+    return false;
   }),
+
+  isDisabled: equal('disabled', true),
 
   click() {
     this.sendAction('onCheck', this.get('value'));
