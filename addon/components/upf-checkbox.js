de32ff0e3adfb6c160b84aa39e6223dac2e29633
Philippe Ndiaye <phndiaye@gmail.com>
2019-09-13 16:34:44 +0200

    add support for size small checkbox

--- addon/components/upf-checkbox.js
+++ addon/components/upf-checkbox.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
+import { equal } from '@ember/object/computed';
 
 export default Component.extend({
   classNames: ['upf-checkbox'],
-  classNameBindings: ['hasLabel:upf-checkbox--has-label']
+  classNameBindings: [
+    'hasLabel:upf-checkbox--has-label', 'sizeSmall:upf-checkbox--sm'
+  ],
+
+  sizeSmall: equal('size', 'sm')
 });
