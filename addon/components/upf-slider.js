8d24b55c22d18d0708c98704293516259ca4652b
Mohamed Hegazy <m.hegazy94@hotmail.com>
2020-01-13 16:46:44 +0100

    Change requests

--- addon/components/upf-slider.js
+++ addon/components/upf-slider.js
@@ -26,14 +26,13 @@ export default Component.extend({
       min: this.options.min,
       max: this.options.max,
       from: this.options.max/2,
-      force_edges: true,
       hide_min_max: true,
       onChange: (data) => {
         if(!this.active) {
           this.toggleProperty('active');
         }
         this.set('value', data.from);
-        this.options.onChange(data.from);
+        this.onChange(data.from);
       },
       prettify: this.formatValue || null
     });
