fc0dd861b84218b306b4748e4cc617f08a4a022c
Mohamed Hegazy <m.hegazy94@hotmail.com>
2020-05-28 16:23:21 +0200

    Remove unused action

--- addon/components/upf-rating.js
+++ addon/components/upf-rating.js
@@ -2,10 +2,4 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['upf-rating'],
-
-  actions: {
-    saveRatingValue(rating) {
-      this.influencer.set('rating', rating);
-    }
-  }
 });
