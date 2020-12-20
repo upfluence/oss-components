b6a0b6ef615c794497b97dd92fdf80fce9635122
Philippe Ndiaye <phndiaye@gmail.com>
2020-12-20 20:27:36 +0100

    fine tune the stories/story categories order

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,13 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
-      order: ['Getting Started', 'Components']
+      order: [
+        'Getting Started',
+        'Core',
+          ['Colors', 'Typography', 'Icons', 'Variables'],
+        'Base',
+        'Components'
+      ]
     }
   }
 }
