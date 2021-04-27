683ae784be8548b16a3b0c7c2bb09935ca105713
Julien Vannier <julien.vannier66@gmail.com>
2021-04-27 18:33:28 +0200

    Add tooltip modifier

---
+++ addon/modifiers/enable-tooltip.js
@@ -0,0 +1,30 @@
+import { setModifierManager } from '@ember/modifier';
+import $ from 'jquery';
+
+export default setModifierManager(
+  () => ({
+    createModifier() {
+      return {
+        element: null
+      };
+    },
+
+    installModifier(state, element, args) {
+      state.element = element;
+      args = args.named;
+
+      $(element).tooltip({
+        delay: {show: 300},
+        placement: args.placement || 'bottom',
+        title: args.title || '',
+        html: args.html || false
+      });
+    },
+
+    destroyModifier(state) {
+      // We don't need to do anything here, but a function
+      // still has to be here so we'll leave it blank.
+    }
+  }),
+  class EnableTooltipModifierManager {}
+);
