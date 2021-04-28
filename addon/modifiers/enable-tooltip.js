0000b7047af92d12347ab8dbea138712896f6b89
Julien Vannier <julien.vannier66@gmail.com>
2021-04-28 12:07:38 +0200

    Add function when tooltip need to be updated

--- addon/modifiers/enable-tooltip.js
+++ addon/modifiers/enable-tooltip.js
@@ -22,6 +22,13 @@ export default setModifierManager(
       });
     },
 
+    updateModifier(state, args) {
+      //We use this function when tooltip title is updated and depends to translation with value
+      args = args.named;
+
+      $(state.element).attr('data-original-title', args.title);
+    },
+
     destroyModifier(state) {
       // We don't need to do anything here, but a function
       // still has to be here so we'll leave it blank.
