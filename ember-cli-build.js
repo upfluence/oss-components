896dc18c39afabdbfcff4cd535ecc01ebf555b4d
Philippe Ndiaye <phndiaye@gmail.com>
2021-07-05 10:19:10 +0200

    oss/modal: add tests

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    tests: false,
-
     lessoptions: {
       paths: [
         'node_modules/upfluence-oss-components/app/styles',
