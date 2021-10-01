e230a6f6bdce1f7be1ddc66c1b979276464ba60a
Philippe Ndiaye <phndiaye@gmail.com>
2021-10-01 08:15:26 +0200

    more typing and small refactor

--- addon/services/toast.ts
+++ addon/services/toast.ts
@@ -11,11 +11,13 @@ enum ToastType {
   SUCCESS = 'success'
 }
 
-type ToastOptions = {
+export type ToastOptions = {
   closeButton?: true;
   tapToDismiss?: boolean;
   onclick?: Function | null;
 };
+
+type ToastFunction = (message: string, title: string, opts: ToastOptions) => void;
 
 const DEFAULT_OPTIONS: ToastOptions = Object.freeze({
   closeButton: true,
@@ -23,11 +25,10 @@ const DEFAULT_OPTIONS: ToastOptions = Object.freeze({
   tapToDismiss: true
 });
 
-function delegate(ctx: Toast, type: ToastType): Function {
+function delegate(ctx: Toast, type: ToastType): ToastFunction {
   return (message: string, title: string, opts: ToastOptions = DEFAULT_OPTIONS): void => {
     if (isEmpty(message) && isEmpty(title)) return;
 
-    console.log(opts);
     ctx.buildToast(type, message, title, { ...DEFAULT_OPTIONS, ...opts });
   };
 }
@@ -35,40 +36,39 @@ function delegate(ctx: Toast, type: ToastType): Function {
 export default class Toast extends Service {
   private _toasts: Element[] = [];
 
-  success = delegate(this, ToastType.SUCCESS);
-  error = delegate(this, ToastType.ERROR);
-  warning = delegate(this, ToastType.WARNING);
-  info = delegate(this, ToastType.INFO);
+  success: ToastFunction = delegate(this, ToastType.SUCCESS);
+  error: ToastFunction = delegate(this, ToastType.ERROR);
+  warning: ToastFunction = delegate(this, ToastType.WARNING);
+  info: ToastFunction = delegate(this, ToastType.INFO);
 
   buildToast(type: ToastType, message: string, title: string, opts: ToastOptions): void {
-    const container = this._buildContainer();
-    console.log(opts);
+    const container: Element = this._buildContainer();
 
-    const toast = document.createElement('div');
+    const toast: Element = document.createElement('div');
     toast.classList.add('toast', 'toast--hidden', `toast-${type}`);
 
-    const titleContainer = document.createElement('div');
+    const titleContainer: Element = document.createElement('div');
     titleContainer.classList.add('toast-title');
     titleContainer.textContent = title;
 
-    const messageContainer = document.createElement('div');
+    const messageContainer: Element = document.createElement('div');
     messageContainer.classList.add('toast-message');
     messageContainer.textContent = message;
 
     let toastChildren: Element[] = [titleContainer, messageContainer];
 
     if (opts.closeButton) {
-      const closeButton = document.createElement('button');
+      const closeButton: Element = document.createElement('button');
 
       closeButton.classList.add('toast-close-button');
       closeButton.innerHTML = '<i class="fas fa-times text-size-5"></i>';
-      closeButton.addEventListener('click', this._onToastClose.bind(this), false);
+      closeButton.addEventListener('click', this._onToastClose.bind(this), { once: true });
 
       toastChildren.push(closeButton);
     }
 
     if (opts.tapToDismiss) {
-      toast.addEventListener('click', () => this._destroyToast(toast), false);
+      toast.addEventListener('click', () => this._destroyToast(toast), { once: true });
     }
 
     toastChildren.forEach((x) => toast.append(x));
@@ -92,31 +92,22 @@ export default class Toast extends Service {
     later(this, () => this._destroyToast(toast), TIMEOUT);
   }
 
-  private _destroyToast(toast: Element | HTMLElement | null): void {
-    if (toast) {
-      toast.removeEventListener('click', () => this._destroyToast(toast), false);
-      toast.remove();
-
-      let container = document.querySelector('body #toast-container.upf-toastr--container');
-      if (container?.children.length === 0) {
-        container.remove();
-      }
-    }
+  private _destroyToast(toast: Element | null): void {
+    toast?.remove();
   }
 
-  private _onToastClose(evt: Event) {
+  private _onToastClose(evt: Event): void {
     evt.stopPropagation();
 
     const button = (<Element>evt.target).tagName === 'I' ? (<Element>evt.target).parentElement : <Element>evt.target;
 
     if (button) {
-      button.removeEventListener('click', this._onToastClose, false);
       this._destroyToast(button.parentElement);
     }
   }
 
   private _buildContainer(): Element {
-    let container = document.querySelector('body #toast-container.upf-toastr--container');
+    let container: Element | null = document.querySelector('body #toast-container.upf-toastr--container');
 
     if (!container) {
       container = document.createElement('div');
