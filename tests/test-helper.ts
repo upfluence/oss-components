6e8a73a10f81c730c453c210f6cbc23d89ea2238
MBrunelle <maximilien.brunelle@gmail.com>
2021-10-15 17:29:50 +0200

    Added: component tests

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -2,9 +2,12 @@
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import 'qunit-dom';
 
+setup(QUnit.assert);
 setApplication(Application.create(config.APP));
 
 start();
