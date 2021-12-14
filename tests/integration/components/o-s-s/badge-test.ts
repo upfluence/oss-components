b4bc6a1ae280a18045412e1ab601e45f4f2700aa
Philippe Ndiaye <phndiaye@gmail.com>
2021-12-14 10:39:34 +0100

    add tests

--- tests/integration/components/o-s-s/badge-test.ts
+++ tests/integration/components/o-s-s/badge-test.ts
@@ -1,26 +1,79 @@
-//import { module, test } from 'qunit';
-//import { setupRenderingTest } from 'ember-qunit';
-//import { render } from '@ember/test-helpers';
-//import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, setupOnerror } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
-//module('Integration | Component | o-s-s/badge', function(hooks) {
-  //setupRenderingTest(hooks);
+module('Integration | Component | o-s-s/badge', function (hooks) {
+  setupRenderingTest(hooks);
 
-  //test('it renders', async function(assert) {
-    //// Set any properties with this.set('myProperty', 'value');
-    //// Handle any actions with this.set('myAction', function(val) { ... });
+  test('it throws an error if no argument is passed', async function (assert: Assert) {
+    setupOnerror((err: Error) => {
+      assert.equal(
+        err.message,
+        '[component][OSS::Badge] One of @icon, @image or @text arguments is mandatory. You passed 0 arguments'
+      );
+    });
 
-    //await render(hbs`{{o-s-s/badge}}`);
+    await render(hbs`<OSS::Badge />`);
+  });
 
-    //assert.equal(this.element.textContent.trim(), '');
+  test('it throws an error if more than one content argument is passed', async function (assert: Assert) {
+    setupOnerror((err: Error) => {
+      assert.equal(
+        err.message,
+        '[component][OSS::Badge] One of @icon, @image or @text arguments is mandatory. You passed 2 arguments'
+      );
+    });
 
-    //// Template block usage:
-    //await render(hbs`
-      //{{#o-s-s/badge}}
-        //template block text
-      //{{/o-s-s/badge}}
-    //`);
+    await render(hbs`<OSS::Badge @icon="fas fa-users" @text="2x" />`);
+  });
 
-    //assert.equal(this.element.textContent.trim(), 'template block text');
-  //});
-//});
+  module('sizes', function () {
+    test('it throws an error when an unsupported size is passed', async function (assert: Assert) {
+      setupOnerror((err: Error) => {
+        assert.equal(err.message, '[component][OSS::Badge] Unknown size. Available sizes are: sm, md, lg');
+      });
+
+      await render(hbs`<OSS::Badge @size="foo" @text="2x" />`);
+    });
+
+    test('it sets the right class when usng a supported size', async function (assert: Assert) {
+      await render(hbs`<OSS::Badge @size="lg" @text="2x" />`);
+
+      assert.dom('.upf-badge').exists();
+      assert.dom('.upf-badge').hasClass('upf-badge--size-lg');
+    });
+
+    test('it defaults to md size if none is passed', async function (assert: Assert) {
+      await render(hbs`<OSS::Badge @text="2x" />`);
+
+      assert.dom('.upf-badge').exists();
+      assert.dom('.upf-badge').hasClass('upf-badge--size-md');
+    });
+  });
+
+  module('content args', function () {
+    test('it displays the right icon when using the @icon arg', async function (assert: Assert) {
+      await render(hbs`<OSS::Badge @icon="fas fa-users" />`);
+
+      assert.dom('.upf-badge').exists();
+      assert.dom('.upf-badge i').hasAttribute('class', 'fas fa-users text-color-bright-purple');
+    });
+
+    test('it displays the right text when using the @text arg', async function (assert: Assert) {
+      await render(hbs`<OSS::Badge @text="2x" />`);
+
+      assert.dom('.upf-badge').exists();
+      assert.dom('.upf-badge .upf-badge__text').exists();
+      assert.dom('.upf-badge .upf-badge__text').hasText('2x');
+    });
+
+    test('it displays the right image when using the @image arg', async function (assert: Assert) {
+      await render(hbs`<OSS::Badge @image="http://foo.co/bar.png" />`);
+
+      assert.dom('.upf-badge').exists();
+      assert.dom('.upf-badge img').exists();
+      assert.dom('.upf-badge img').hasAttribute('src', 'http://foo.co/bar.png')
+    });
+  });
+});
