dfac65a2192074940a011530db0814d2ba0b013f
MBrunelle <maximilien.brunelle@gmail.com>
2021-12-23 14:45:49 +0100

    Fixed: PR Comment

--- tests/integration/components/o-s-s/star-rating-test.ts
+++ tests/integration/components/o-s-s/star-rating-test.ts
@@ -32,6 +32,7 @@ module('Integration | Component | o-s-s/star-rating', function (hooks) {
   for (const starColor in StarColor) {
     test('Passing @activeColor and @passiveColor applies the correct classes to the component', async function (assert) {
       this.activeColor = starColor;
+      this.passiveColor = starColor;
       await render(hbs`<OSS::StarRating @rating={{5}} @totalStars={{8}}
                                         @activeColor={{this.activeColor}}
                                         @passiveColor={{this.passiveColor}} />`);
