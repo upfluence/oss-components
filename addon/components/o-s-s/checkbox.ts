6048e89faa2442a9b4a120284248c672b981fe6c
Philippe Ndiaye <phndiaye@gmail.com>
2022-01-18 17:10:04 +0100

    add OSS::Checkbox component

---
+++ addon/components/o-s-s/checkbox.ts
@@ -0,0 +1,55 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
+
+const AVAILABLE_SIZES = ['sm'];
+
+interface OSSCheckboxArgs {
+  checked: boolean;
+  disabled: boolean;
+  size: string;
+  onChange(value: boolean): void;
+}
+
+export default class OSSCheckbox extends Component<OSSCheckboxArgs> {
+  constructor(owner: unknown, args: OSSCheckboxArgs) {
+    super(owner, args);
+
+    if (typeof args.checked !== 'boolean') {
+      throw new Error(`[component][OSS::Checkbox] Boolean @checked argument is mandatory.`);
+    }
+
+    if (!args.onChange) {
+      throw new Error(`[component][OSS::Checkbox] @onChange argument is mandatory.`);
+    }
+  }
+
+  get elementId(): string {
+    return guidFor(this);
+  }
+
+  get modifierClasses(): string {
+    const classes = [];
+
+    if (this.args.disabled) {
+      classes.push('upf-checkbox--disabled');
+    }
+
+    if (this.args.size && AVAILABLE_SIZES.includes(this.args.size)) {
+      classes.push(`upf-checkbox--${this.args.size}`);
+    }
+
+    return classes.join(' ');
+  }
+
+  @action
+  noOp(event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (!this.args.disabled) {
+      console.log('f')
+      this.args.onChange(!this.args.checked);
+    }
+  }
+}
---
+++ tests/integration/components/o-s-s/checkbox-test.ts
@@ -0,0 +1,60 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render, setupOnerror } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
+
+module('Integration | Component | o-s-s/checkbox', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.onChange = () => {};
+  });
+
+  test('it throws an error if checked argument is missing', async function (assert) {
+    setupOnerror((error: Error) => {
+      assert.equal(error.message, '[component][OSS::Checkbox] Boolean @checked argument is mandatory.');
+    });
+    await render(hbs`<OSS::Checkbox />`);
+  });
+
+  test('it throws an error if checked argument is missing', async function (assert) {
+    setupOnerror((error: Error) => {
+      assert.equal(error.message, '[component][OSS::Checkbox] @onChange argument is mandatory.');
+    });
+
+    await render(hbs`<OSS::Checkbox @checked={{true}} />`);
+  });
+
+  test('it renders correctly', async function (assert: Assert) {
+    this.checked = false;
+
+    await render(hbs`<OSS::Checkbox @checked={{this.checked}} @onChange={{this.onChange}} />`);
+
+    assert.dom('.upf-checkbox input').isNotChecked();
+    this.set('checked', true);
+    assert.dom('.upf-checkbox input').isChecked();
+  });
+
+  test('disabled state is handled correctly', async function (assert: Assert) {
+    const onChangeSpy = sinon.spy(this.onChange);
+    await render(hbs`<OSS::Checkbox @checked={{false}} @onChange={{this.onChange}} @disabled={{true}} />`);
+    assert.dom('.upf-checkbox input').hasAttribute('disabled');
+
+    await click('.upf-checkbox');
+
+    assert.ok(onChangeSpy.neverCalledWith())
+  });
+
+  test('the onChange hooks is called with the new value', async function(assert: Assert) {
+    this.checked = false;
+    this.onChange = (value: boolean) => {
+      assert.equal(value, true);
+    }
+
+    await render(hbs`<OSS::Checkbox @checked={{this.checked}} @onChange={{this.onChange}} />`);
+    await click('.upf-checkbox');
+
+    assert.expect(1);
+  })
+});
