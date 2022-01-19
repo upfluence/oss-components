26412f9f2faebb7e1d7c5c3a79d7eb74a60e240f
Philippe Ndiaye <phndiaye@gmail.com>
2022-01-19 16:28:06 +0100

    fix on-bottom-reached

--- addon/modifiers/on-bottom-reached.ts
+++ addon/modifiers/on-bottom-reached.ts
@@ -4,10 +4,7 @@ import { setModifierManager } from '@ember/modifier';
 type State = {
   element: Element;
   handler: (...args: unknown[]) => unknown;
-  cachedScrollTop: number;
 };
-
-const TRIGGER_OFFSET = 20;
 
 /**
  * Used to trigger an action a user reaches the bottom of the modified element.
@@ -25,14 +22,9 @@ export default setModifierManager(
     installModifier(state: State, element: Element, { positional }: any) {
       const [callback] = positional;
       state.element = element;
-      state.cachedScrollTop = 0;
 
-      state.handler = (_: Event) => {
-        if (
-          state.element.scrollTop >= state.cachedScrollTop &&
-          Math.ceil(state.element.scrollTop) + state.element.clientHeight >= state.element.scrollHeight - TRIGGER_OFFSET
-        ) {
-          state.cachedScrollTop = state.element.scrollTop + TRIGGER_OFFSET;
+      state.handler = (event: Event) => {
+        if (Math.ceil(state.element.scrollTop) + state.element.clientHeight >= state.element.scrollHeight) {
           callback(state.element, event);
         }
       };
