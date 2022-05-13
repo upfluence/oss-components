fff2851ad6b8cb5da69648e7c4e78d7d4c0d53f7
MBrunelle <maximilien.brunelle@gmail.com>
2022-05-13 17:55:11 +0200

    Added: animation + scroll fix

--- addon/components/o-s-s/modal-dialog.ts
+++ addon/components/o-s-s/modal-dialog.ts
@@ -10,6 +10,8 @@ interface OSSModalDialogArgs {
 
 export default class OSSModalDialog extends Component<OSSModalDialogArgs> {
   private declare _elem: HTMLElement;
+  private prevBodyOverflow: string | null = null;
+  private prevBodyPadding: string | null = null;
 
   constructor(owner: unknown, args: OSSModalDialogArgs) {
     super(owner, args);
@@ -32,12 +34,24 @@ export default class OSSModalDialog extends Component<OSSModalDialogArgs> {
   @action
   destroy(): void {
     document.removeEventListener('keyup', this._closeOnEscape);
+    document.body.style.overflow = this.prevBodyOverflow || 'auto';
+    document.body.style.paddingRight = this.prevBodyPadding || '0';
+  }
+
+  scrollbarVisible(): boolean {
+    return window.innerWidth > document.documentElement.clientWidth;
   }
 
   @action
   init(elem: HTMLElement): void {
     this._elem = elem;
     elem.classList.add('show-modal');
+    if (this.scrollbarVisible()) {
+      this.prevBodyOverflow = document.body.style.overflow;
+      document.body.style.overflow = 'hidden';
+      this.prevBodyPadding = document.body.style.paddingRight;
+      document.body.style.paddingRight = '16px';
+    }
   }
 
   @action
