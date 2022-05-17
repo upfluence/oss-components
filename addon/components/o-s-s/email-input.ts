3065fd517c03a9d33b2a32df160afd41bd914b02
Philippe Ndiaye <phndiaye@gmail.com>
2022-05-17 12:20:17 +0200

    update email regexp to rfc standard

--- addon/components/o-s-s/email-input.ts
+++ addon/components/o-s-s/email-input.ts
@@ -13,7 +13,9 @@ interface OSSEmailInputArgs {
 
 const DEFAULT_PLACEHOLDER = 'e.g: john.doe@example.com';
 
-export const EMAIL_REGEXP = new RegExp(/^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/);
+export const EMAIL_REGEXP = new RegExp(
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+);
 
 export default class OSSEmailInput extends Component<OSSEmailInputArgs> {
   @service intl: any;
@@ -46,7 +48,7 @@ export default class OSSEmailInput extends Component<OSSEmailInputArgs> {
   validateInput(): void {
     this.regexError = '';
 
-    if (!this._runValidation || !this.args.value) {
+    if (!this._runValidation || !this.args.value) {
       this.args.validates?.(true);
     } else if (!EMAIL_REGEXP.test(this.args.value)) {
       this.regexError = this.intl.t('oss-components.email-input.regex_error');
