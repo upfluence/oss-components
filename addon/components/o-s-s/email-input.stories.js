dc3f37d884091a19c579fe2b219a0f9006d27140
Philippe Ndiaye <phndiaye@gmail.com>
2022-05-17 14:51:21 +0200

    Add support for email input component (#165)
    * add support for email input component
    
    * email input: add tests
    
    * email input: add doc
    
    * copy/paste fix
    
    * fix pr feedbacks
    
    * update email regexp to rfc standard

---
+++ addon/components/o-s-s/email-input.stories.js
@@ -0,0 +1,67 @@
+import hbs from 'htmlbars-inline-precompile';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Components/OSS::EmailInput',
+  component: 'email-input',
+  argTypes: {
+    value: {
+      description: 'Value of the email input',
+      table: {
+        type: {
+          summary: 'string'
+        },
+        defaultValue: { summary: false }
+      },
+      control: { type: 'text' }
+    },
+    placeholder: {
+      description: 'Value of the placeholder',
+      table: {
+        type: {
+          summary: 'string'
+        },
+        defaultValue: { summary: '*************' }
+      },
+      control: { type: 'text' }
+    },
+    errorMessage: {
+      description: 'Error message that is displayed when the email pattern is invalid',
+      table: {
+        type: {
+          summary: 'string'
+        },
+        defaultValue: { summary: false }
+      },
+      control: { type: 'text' }
+    },
+    validateFormat: {
+      description: 'Whether or not to validate the email format with the RegEx',
+      table: {
+        type: {
+          summary: 'boolean'
+        },
+        defaultValue: { summary: true }
+      },
+      control: { type: 'boolean' }
+    },
+    validates: {
+      description: 'A callback that indicates wheter or not the current input matches the regex'
+    }
+  }
+};
+
+const DefaultUsageTemplate = (args) => ({
+  template: hbs`
+      <OSS::EmailInput @value={{this.value}} @placeholder={{this.placeholder}} @validateFormat={{this.validateFormat}} @validates={{this.validates}} />
+  `,
+  context: args
+});
+export const BasicUsage = DefaultUsageTemplate.bind({});
+BasicUsage.args = {
+  value: 'john.doe@example.com',
+  placeholder: 'foo@bar.org',
+  errorMessage: '',
+  validateFormat: false,
+  validates: action('validates')
+};
---
+++ addon/components/o-s-s/email-input.ts
@@ -0,0 +1,60 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+interface OSSEmailInputArgs {
+  value: string | null;
+  placeholder?: string;
+  errorMessage?: string;
+  validateFormat?: boolean;
+  validates?(isPassing: boolean): void;
+}
+
+const DEFAULT_PLACEHOLDER = 'e.g: john.doe@example.com';
+
+export const EMAIL_REGEXP = new RegExp(
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+);
+
+export default class OSSEmailInput extends Component<OSSEmailInputArgs> {
+  @service intl: any;
+
+  private _runValidation: boolean = true;
+
+  @tracked regexError: string | null = null;
+
+  constructor(owner: unknown, args: OSSEmailInputArgs) {
+    super(owner, args);
+
+    if (typeof args.value === 'undefined') {
+      throw new Error('[component][OSS::EmailInput] The @value parameter is mandatory');
+    }
+
+    if (typeof args.validateFormat === 'boolean') {
+      this._runValidation = args.validateFormat;
+    }
+  }
+
+  get placeholder(): string {
+    return this.args.placeholder ?? DEFAULT_PLACEHOLDER;
+  }
+
+  get errorMessage(): string | null {
+    return this.args.errorMessage || this.regexError || null;
+  }
+
+  @action
+  validateInput(): void {
+    this.regexError = '';
+
+    if (!this._runValidation || !this.args.value) {
+      this.args.validates?.(true);
+    } else if (!EMAIL_REGEXP.test(this.args.value)) {
+      this.regexError = this.intl.t('oss-components.email-input.regex_error');
+      this.args.validates?.(false);
+    } else {
+      this.args.validates?.(true);
+    }
+  }
+}
