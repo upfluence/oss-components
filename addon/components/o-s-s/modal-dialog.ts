c4a647c569b010158014e10fac93bc03c07f1643
MaximilienBrunelle <maximilien.brunelle@gmail.com>
2022-05-19 15:20:34 +0200

    Fixed: ModalDialogs are now appended to body

--- addon/components/o-s-s/modal-dialog.ts
+++ addon/components/o-s-s/modal-dialog.ts
@@ -10,6 +10,7 @@ interface OSSModalDialogArgs {
 
 export default class OSSModalDialog extends Component<OSSModalDialogArgs> {
   private declare _elem: HTMLElement;
+  private declare _parent: HTMLElement;
   private prevBodyOverflow: string | null = null;
   private prevBodyPadding: string | null = null;
 
@@ -33,6 +34,7 @@ export default class OSSModalDialog extends Component<OSSModalDialogArgs> {
 
   @action
   destroy(): void {
+    this._parent.remove();
     document.removeEventListener('keyup', this._closeOnEscape);
     document.body.style.overflow = this.prevBodyOverflow || 'auto';
     document.body.style.paddingRight = this.prevBodyPadding || '0';
@@ -44,7 +46,6 @@ export default class OSSModalDialog extends Component<OSSModalDialogArgs> {
 
   @action
   init(elem: HTMLElement): void {
-    this._elem = elem;
     elem.classList.add('show-modal');
     if (this.scrollbarVisible()) {
       this.prevBodyOverflow = document.body.style.overflow;
@@ -52,6 +53,11 @@ export default class OSSModalDialog extends Component<OSSModalDialogArgs> {
       this.prevBodyPadding = document.body.style.paddingRight;
       document.body.style.paddingRight = '16px';
     }
+    if (elem.parentElement) {
+      this._parent = elem.parentElement;
+      document.body.append(this._parent);
+    }
+    this._elem = elem;
   }
 
   @action
