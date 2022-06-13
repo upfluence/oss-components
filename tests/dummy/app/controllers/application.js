dd9adc6b6ea66f9bd962aebc0e1f70074e2c23bd
MaximilienBrunelle <maximilien.brunelle@gmail.com>
2022-06-13 16:37:42 +0200

    Added: CountrySelector component without tests

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,10 +2,14 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { countries } from '@upfluence/oss-components/utils/country-codes';
 
 export default class ApplicationController extends Controller {
   @service toast;
 
+  @tracked selectedItems = ['toto'];
+  @tracked selectedCountry: { name: string, id?: string } = null;
+  @tracked superHeroes = ['Iron Man', 'Thor', 'Loki', 'Captain America', 'Hulk'];
   @tracked loading = false;
   @tracked phonePrefix = '+33';
   @tracked phoneNumber = '782828282';
@@ -14,6 +18,7 @@ export default class ApplicationController extends Controller {
   @tracked showModal = false;
 
   code4CodeBlock = testScript;
+  countries = countries;
 
   @action
   openModal(e) {
@@ -24,6 +29,17 @@ export default class ApplicationController extends Controller {
   @action
   closeModal() {
     this.showModal = false;
+  }
+
+  @action
+  onCountrySelected(value) {
+    console.log('selected country value : ', value);
+    this.selectedCountry = value;
+  }
+
+  @action
+  onProvinceSelected(value) {
+    console.log('selected province value : ', value);
   }
 
   @action
