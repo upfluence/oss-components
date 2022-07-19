14b86eb306c01995729043507607c100e180f9f0
sBenjamin270495 <104398916+sBenjamin270495@users.noreply.github.com>
2022-07-19 17:58:13 +0200

    Update after pr review

--- addon/components/o-s-s/chip.stories.js
+++ addon/components/o-s-s/chip.stories.js
@@ -7,7 +7,6 @@ export default {
   title: 'Components/OSS::Chip',
   component: 'tag',
   argTypes: {
-
     skin: {
       description: 'Adjust appearance',
       table: {
@@ -48,7 +47,8 @@ export default {
     },
 
     ellipsisWidth: {
-      description: 'Max width before we apply ellispsis on text : Requirement: <br/> -be careful you have to had enabled-tooltip for hover on chip <br/> -only few class exist for ellispsis',
+      description:
+        'Max width before we apply ellispsis on text : Requirement: <br/> -be careful you have to had enabled-tooltip for hover on chip <br/> -only few class exist for ellispsis',
       table: {
         type: { summary: 'number' },
         defaultValue: { summary: 'undefined' }
