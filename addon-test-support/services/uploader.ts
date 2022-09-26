23bed93dfd196df92701754f2866acfb72489fac
Philippe Ndiaye <phndiaye@gmail.com>
2022-09-26 10:42:45 +0200

    add support for a mock uploader service

---
+++ addon-test-support/services/uploader.ts
@@ -0,0 +1,44 @@
+import BaseUploader from '@upfluence/oss-components/services/base-uploader';
+import UploaderInterface, {
+  UploadRequest,
+  UploadRule,
+  ValidationResponse
+} from '@upfluence/oss-components/types/uploader';
+
+export default class extends BaseUploader implements UploaderInterface {
+  mode: 'success' | 'failure' = 'success';
+
+  upload(request: UploadRequest, validationRules?: UploadRule[]): void {
+    const validations: ValidationResponse = this.validate(request, validationRules || []);
+
+    if (!validations.passes) {
+      request.onValidationFailure?.(validations);
+      return;
+    }
+
+    [250, 500, 750, 1000].forEach((progressStep: number) => {
+      const progressEvent = new ProgressEvent('progress', { total: 1000, loaded: progressStep });
+      setTimeout(() => {
+        request.onProgress?.(progressEvent);
+      }, 100);
+    });
+
+    if (this.mode === 'success') {
+      request.onSuccess?.({
+        key: 'uploader/foo.png',
+        filename: 'Foo.png',
+        url: 'https://oss-components.upfluence.co/uploader/foo.png',
+        content_type: 'png',
+        size: 1000
+      });
+    } else {
+      request.onFailure?.({
+        payload: {}
+      });
+    }
+  }
+
+  get url() {
+    return 'https://oss-components.upfluence.co';
+  }
+}
