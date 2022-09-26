db7702af77b3b01746ddd3d4ba92d903cde916d0
benjamin <sbenjamin2704@gmail.com>
2022-09-26 12:03:50 +0200

    Add test of OSS::Avatar component

--- addon/components/o-s-s/avatar.ts
+++ addon/components/o-s-s/avatar.ts
@@ -5,7 +5,7 @@ import { action } from '@ember/object';
 
 type SizeType = 'xs' | 'sm' | 'md' | 'lg';
 type SizeDefType = { [key in SizeType]: string };
-const SizeDefinition: SizeDefType = {
+export const SizeDefinition: SizeDefType = {
   xs: 'upf-avatar--xs',
   sm: 'upf-avatar--sm',
   md: 'upf-avatar--md',
@@ -18,15 +18,17 @@ interface OSSAvatarArgs {
   size?: SizeType;
 }
 
-const DEFAULT_IMAGE_URL: string = '/assets/images/upfluence-white-logo.svg';
+export const DEFAULT_IMAGE_URL: string = '/assets/images/upfluence-white-logo.svg';
 
 export default class OSSAvatar extends Component<OSSAvatarArgs> {
   @tracked hasError: boolean = false;
   @tracked displayInitial: boolean = false;
+
   constructor(owner: unknown, args: OSSAvatarArgs) {
     super(owner, args);
     if (!args.image && args.initials) this.displayInitial = true;
   }
+
   get image(): string | undefined {
     return !this.args.image && !this.args.initials ? DEFAULT_IMAGE_URL : this.args.image;
   }
@@ -34,17 +36,19 @@ export default class OSSAvatar extends Component<OSSAvatarArgs> {
   get imageClass(): string {
     return this.image === DEFAULT_IMAGE_URL || this.hasError ? 'img-placeholder' : '';
   }
+
   get computedClass(): string {
     const classes = 'upf-avatar ';
     const size: SizeType = this.args.size || 'md';
 
     assert(
-      `[component][OSS::Badge] Unknown size. Available sizes are: ${Object.keys(SizeDefinition).join(', ')}`,
+      `[component][OSS::Avatar] Unknown size. Available sizes are: ${Object.keys(SizeDefinition).join(', ')}`,
       Object.keys(SizeDefinition).includes(size as SizeType)
     );
 
     return classes.concat(SizeDefinition[size as SizeType]);
   }
+
   @action
   imageLoadError(event: Event): void {
     if (this.args.initials) {
