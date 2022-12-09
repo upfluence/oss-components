3310442608f8013c29c370f30bbed774edbe2786
Julien Vannier <julien.vannier66@gmail.com>
2022-12-09 18:31:00 +0100

    Add djoo utils

---
+++ addon/utils/djoo.ts
@@ -0,0 +1,196 @@
+//Dynamic Javascript Orientation Organizer
+// @ts-nocheck
+
+export type PlacementType = 'top' | 'bottom' | 'right' | 'left';
+type ElementOptions = {
+  placement?: string; // the position of the element in relation to the target
+  cssVariableName: string; // name of the custom css variable
+  elementTargetMargin: number; // the margin value between the element and the target
+  viewPortPadding: number; // the padding relative to the viewport
+};
+type ArrowOptions = {
+  defaultRotation: number; // defaultRotation
+  height: number; //height of the arrow
+  width: number; //height of the arrow
+};
+
+const CSS_VARIABLE_NAME_PREFIX = '--upf-';
+
+export class Djoo {
+  declare elementOptions: ElementOptions;
+  declare arrowOptions: ArrowOptions;
+
+  hasVerticalAdjustment: boolean = false;
+  hasHorizontalAdjustment: boolean = false;
+
+  computePosition(
+    element: HTMLElement,
+    target: HTMLElement,
+    elementOptions: ElementOptions,
+    arrowOptions: ArrowOptions
+  ): void {
+    this.elementOptions = elementOptions;
+    this.arrowOptions = arrowOptions;
+
+    const elementTop = this.computedElementTop(element, target);
+    element.style.setProperty(
+      `${CSS_VARIABLE_NAME_PREFIX}${this.elementOptions.cssVariableName}-top`,
+      `${elementTop}px`
+    );
+    const elementLeft = this.computedElementLeft(element, target);
+    element.style.setProperty(
+      `${CSS_VARIABLE_NAME_PREFIX}${this.elementOptions.cssVariableName}-left`,
+      `${elementLeft}px`
+    );
+
+    this.verticalAdjustmentRelativeToViewPort(element);
+    this.horizontalAdjustmentRelativeToViewPort(element);
+
+    if (this.arrowOptions) {
+      const arrowLeft = this.computedArrowLeft(element, target);
+      element.style.setProperty(
+        `${CSS_VARIABLE_NAME_PREFIX}${this.elementOptions.cssVariableName}-arrow-left`,
+        `${arrowLeft}px`
+      );
+      const arrowTop = this.computedArrowTop(element, target);
+      element.style.setProperty(
+        `${CSS_VARIABLE_NAME_PREFIX}${this.elementOptions.cssVariableName}-arrow-top`,
+        `${arrowTop}px`
+      );
+
+      const rotation = this.computedArrowRotation();
+      element.style.setProperty(
+        `${CSS_VARIABLE_NAME_PREFIX}${this.elementOptions.cssVariableName}-arrow-rotation`,
+        `${rotation}deg`
+      );
+    }
+  }
+
+  private computedElementLeft(element: HTMLElement, target: HTMLElement): number {
+    const targetBoundingClientRect = target.getBoundingClientRect();
+    const targetLeftRelativeToDocument = targetBoundingClientRect.left + document.documentElement.scrollLeft;
+
+    switch (this.elementOptions.placement) {
+      case 'top':
+        return targetLeftRelativeToDocument + target.offsetWidth / 2 - element.offsetWidth / 2;
+      case 'bottom':
+        return targetLeftRelativeToDocument + target.offsetWidth / 2 - element.offsetWidth / 2;
+      case 'right':
+        return targetLeftRelativeToDocument + target.offsetWidth + this.elementOptions.elementTargetMargin;
+      case 'left':
+        return targetLeftRelativeToDocument - element.offsetWidth - this.elementOptions.elementTargetMargin;
+      default:
+        return 0;
+    }
+  }
+
+  private computedElementTop(element: HTMLElement, target: HTMLElement): number {
+    const targetBoundingClientRect = target.getBoundingClientRect();
+    const targetTopRelativeToDocument = targetBoundingClientRect.top + document.documentElement.scrollTop;
+
+    switch (this.elementOptions.placement) {
+      case 'top':
+        return targetTopRelativeToDocument - element.offsetHeight - this.elementOptions.elementTargetMargin;
+      case 'bottom':
+        return targetTopRelativeToDocument + target.offsetHeight + this.elementOptions.elementTargetMargin;
+      case 'right':
+        return targetTopRelativeToDocument + target.offsetHeight / 2 - element.offsetHeight / 2;
+      case 'left':
+        return targetTopRelativeToDocument + target.offsetHeight / 2 - element.offsetHeight / 2;
+      default:
+        return 0;
+    }
+  }
+
+  private verticalAdjustmentRelativeToViewPort(element: HTMLElement): void {
+    if (this.elementOptions.placement !== 'left' && this.elementOptions.placement !== 'right') return;
+
+    const elementBoundingClientRect = element.getBoundingClientRect();
+    if (elementBoundingClientRect.top < this.elementOptions.viewPortPadding) {
+      const currentTopValue = parseInt(element.style.getPropertyValue('--upf-modifier-tooltip-top').replace('px', ''));
+      const correctionValue = elementBoundingClientRect.top - this.elementOptions.viewPortPadding;
+      element.style.setProperty('--upf-modifier-tooltip-top', `${currentTopValue - correctionValue}px`);
+    }
+
+    if (elementBoundingClientRect.bottom > window.innerHeight - this.elementOptions.viewPortPadding) {
+      const currentTopValue = parseInt(element.style.getPropertyValue('--upf-modifier-tooltip-top').replace('px', ''));
+      const correctionValue =
+        elementBoundingClientRect.bottom - window.innerHeight + this.elementOptions.viewPortPadding;
+      element.style.setProperty('--upf-modifier-tooltip-top', `${currentTopValue - correctionValue}px`);
+    }
+  }
+
+  private horizontalAdjustmentRelativeToViewPort(element: HTMLElement): void {
+    if (this.elementOptions.placement !== 'top' && this.elementOptions.placement !== 'bottom') return;
+
+    const elementBoundingClientRect = element.getBoundingClientRect();
+    if (elementBoundingClientRect.left < this.elementOptions.viewPortPadding) {
+      const currentTopValue = parseInt(element.style.getPropertyValue('--upf-modifier-tooltip-left').replace('px', ''));
+      const correctionValue = Math.abs(elementBoundingClientRect.left - this.elementOptions.viewPortPadding);
+      element.style.setProperty('--upf-modifier-tooltip-left', `${currentTopValue + correctionValue}px`);
+    }
+
+    if (
+      elementBoundingClientRect.left + element.offsetWidth - this.elementOptions.viewPortPadding >
+      window.innerWidth
+    ) {
+      const currentTopValue = parseInt(element.style.getPropertyValue('--upf-modifier-tooltip-left').replace('px', ''));
+      const correctionValue =
+        elementBoundingClientRect.left + element.offsetWidth + this.elementOptions.viewPortPadding - window.innerWidth;
+      element.style.setProperty('--upf-modifier-tooltip-left', `${currentTopValue - correctionValue}px`);
+    }
+  }
+
+  private computedArrowLeft(element: HTMLElement, target: HTMLElement): number {
+    const elementBoundingClientRect = element.getBoundingClientRect();
+    const targetBoundingClientRect = target.getBoundingClientRect();
+    const leftRelative = Math.abs(elementBoundingClientRect.left - targetBoundingClientRect.left);
+
+    switch (this.elementOptions.placement) {
+      case 'top':
+        return leftRelative + target.offsetWidth / 2 - this.arrowOptions.width / 2;
+      case 'bottom':
+        return leftRelative + target.offsetWidth / 2 - this.arrowOptions.width / 2;
+      case 'right':
+        return 0 - this.arrowOptions.height;
+      case 'left':
+        return leftRelative - this.elementOptions.elementTargetMargin - this.arrowOptions.height;
+      default:
+        return 0;
+    }
+  }
+
+  private computedArrowTop(element: HTMLElement, target: HTMLElement): number {
+    const elementBoundingClientRect = element.getBoundingClientRect();
+    const targetBoundingClientRect = target.getBoundingClientRect();
+    const topRelative = Math.abs(elementBoundingClientRect.top - targetBoundingClientRect.top);
+
+    switch (this.elementOptions.placement) {
+      case 'top':
+        return topRelative - this.elementOptions.elementTargetMargin - this.arrowOptions.height;
+      case 'bottom':
+        return -this.arrowOptions.height;
+      case 'right':
+        return topRelative + target.offsetHeight / 2 - this.arrowOptions.width / 2;
+      case 'left':
+        return topRelative + target.offsetHeight / 2 - this.arrowOptions.width / 2;
+      default:
+        return 0;
+    }
+  }
+
+  private computedArrowRotation() {
+    switch (this.elementOptions.placement) {
+      case 'top':
+        return this.arrowOptions.defaultRotation;
+      case 'bottom':
+        return this.arrowOptions.defaultRotation + 90 * 2;
+      case 'right':
+        return this.arrowOptions.defaultRotation + 90;
+      case 'left':
+        return this.arrowOptions.defaultRotation + 90 * 3;
+      default:
+        return 0;
+    }
+  }
+}
--- app/modifiers/enable-tooltip-new.js
+++ app/modifiers/enable-tooltip-new.js
@@ -1,81 +1,10 @@
 import { setModifierManager, capabilities } from '@ember/modifier';
-
-const ELEMENT_TARGET_MARGIN = 10;
-
-function _computedLeft(element, target, placement) {
-  const targetBoundingClientRect = target.getBoundingClientRect();
-  const targetLeftRelativeToDocument = targetBoundingClientRect.left + document.documentElement.scrollLeft;
-
-  switch (placement) {
-    case 'top':
-      return targetLeftRelativeToDocument + target.offsetWidth / 2 - element.offsetWidth / 2;
-    case 'bottom':
-      return targetLeftRelativeToDocument + target.offsetWidth / 2 - element.offsetWidth / 2;
-    case 'right':
-      return targetLeftRelativeToDocument + target.offsetWidth + ELEMENT_TARGET_MARGIN;
-    case 'left':
-      return targetLeftRelativeToDocument - element.offsetWidth - ELEMENT_TARGET_MARGIN;
-    default:
-      return 0;
-  }
-}
-
-function _computedTop(element, target, placement) {
-  const targetBoundingClientRect = target.getBoundingClientRect();
-  const targetTopRelativeToDocument = targetBoundingClientRect.top + document.documentElement.scrollTop;
-
-  switch (placement) {
-    case 'top':
-      return targetTopRelativeToDocument - element.offsetHeight - ELEMENT_TARGET_MARGIN;
-    case 'bottom':
-      return targetTopRelativeToDocument + target.offsetHeight + ELEMENT_TARGET_MARGIN;
-    case 'right':
-      return targetTopRelativeToDocument + target.offsetHeight / 2 - element.offsetHeight / 2;
-    case 'left':
-      return targetTopRelativeToDocument + target.offsetHeight / 2 - element.offsetHeight / 2;
-    default:
-      return 0;
-  }
-}
-
-function _verticalAdjustmentRelativeToViewPort(element, placement) {
-  if (placement !== 'left' && placement !== 'right') return;
-
-  const elementBoundingClientRect = element.getBoundingClientRect();
-  if (elementBoundingClientRect.top < 0) {
-    const currentTopValue = element.style.getPropertyValue('--upf-modifier-tooltip-top').replace('px', '');
-    const correctionValue = elementBoundingClientRect.top;
-    element.style.setProperty('--upf-modifier-tooltip-top', `${currentTopValue - correctionValue}px`);
-  }
-
-  if (elementBoundingClientRect.bottom > window.innerHeight) {
-    const currentTopValue = element.style.getPropertyValue('--upf-modifier-tooltip-top').replace('px', '');
-    const correctionValue = elementBoundingClientRect.bottom - window.innerHeight;
-    element.style.setProperty('--upf-modifier-tooltip-top', `${currentTopValue - correctionValue}px`);
-  }
-}
-
-function _horizontalAdjustmentRelativeToViewPort(element, placement) {
-  if (placement !== 'top' && placement !== 'bottom') return;
-
-  const elementBoundingClientRect = element.getBoundingClientRect();
-  if (elementBoundingClientRect.left < 0) {
-    const currentTopValue = element.style.getPropertyValue('--upf-modifier-tooltip-left').replace('px', '');
-    const correctionValue = elementBoundingClientRect.left;
-    element.style.setProperty('--upf-modifier-tooltip-left', `${currentTopValue - correctionValue}px`);
-  }
-
-  if (elementBoundingClientRect.left + element.offsetWidth > window.innerWidth) {
-    const currentTopValue = element.style.getPropertyValue('--upf-modifier-tooltip-left').replace('px', '');
-    const correctionValue = elementBoundingClientRect.left + element.offsetWidth - window.innerWidth;
-    element.style.setProperty('--upf-modifier-tooltip-left', `${currentTopValue - correctionValue}px`);
-  }
-}
+import { Djoo } from '@upfluence/oss-components/utils/djoo';
 
 function _generateHTMLStructure(state, args) {
   const { title, subtitle, icon } = args.named;
   state.tooltip = document.createElement('div');
-  state.tooltip.className = 'upf-tooltip';
+  state.tooltip.className = 'upf-tooltip upf-tooltip--visible';
   let titleContainer = document.createElement('div');
   titleContainer.className = 'title-container';
 
@@ -101,22 +30,26 @@ function _generateHTMLStructure(state, args) {
   state.tooltip.style.minWidth = `${state.tooltip.offsetWidth}px`;
 }
 
-function _generateTooltip(state, args) {
+function _create(state, args) {
   const { placement } = args.named;
+  const djoo = new Djoo();
 
   _generateHTMLStructure(state, args);
 
-  const top = _computedTop(state.tooltip, state.element, placement);
-  state.tooltip.style.setProperty('--upf-modifier-tooltip-top', `${top}px`);
-
-  const left = _computedLeft(state.tooltip, state.element, placement);
-  state.tooltip.style.setProperty('--upf-modifier-tooltip-left', `${left}px`);
-
-  _verticalAdjustmentRelativeToViewPort(state.tooltip, placement);
-  _horizontalAdjustmentRelativeToViewPort(state.tooltip, placement);
+  djoo.computePosition(
+    state.tooltip,
+    state.element,
+    {
+      placement: placement,
+      cssVariableName: 'modifier-tooltip',
+      elementTargetMargin: 9,
+      viewPortPadding: 3
+    },
+    { defaultRotation: -45, height: 4, width: 8.5 }
+  );
 }
 
-function _deleteTooltip(state) {
+function _destroy(state) {
   state.tooltip.remove();
 }
 
@@ -125,6 +58,7 @@ export default setModifierManager(
     capabilities: capabilities('3.13'),
 
     createModifier() {
+      console.log('created');
       return {
         element: null,
         tooltip: null
@@ -132,19 +66,24 @@ export default setModifierManager(
     },
 
     installModifier(state, element, args) {
+      console.log('install');
       state.element = element;
-      _generateTooltip(state, args);
-      /**
-      element.addEventListener('mouseover', () => {
-        _generateTooltip(state, args);
+
+      element.addEventListener('mouseover', (event) => {
+        console.log(event);
+        console.log('create');
+        _create(state, args);
       });
       element.addEventListener('mouseout', () => {
-        _deleteTooltip(state);
-      });**/
+        console.log('destroyed');
+        _destroy(state);
+      });
     },
 
     updateModifier(state, args) {
-      //We use this function when tooltip title is updated and depends to translation with value
+      console.log('update');
+      const { title } = args.named;
+      //TODO
     },
 
     destroyModifier() {
