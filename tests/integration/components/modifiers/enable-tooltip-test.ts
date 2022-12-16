62a5015c795340eefe6e03ab7b507803bca9184c
Julien Vannier <julien.vannier66@gmail.com>
2022-12-16 11:48:39 +0100

    PR feedback & add documentation

--- tests/integration/components/modifiers/enable-tooltip-test.ts
+++ tests/integration/components/modifiers/enable-tooltip-test.ts
@@ -13,43 +13,34 @@ module('Integration | Component | modifiers/enable-tooltip', function (hooks) {
     this.title = 'Title';
   });
 
-  test("it doesn't renders when the title is empty", async function (assert) {
-    this.title = '';
+  async function renderTooltip() {
     await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px" 
-           {{enable-tooltip title=""}}>
+      <div class="test-container" style="height: 20px; width: 40px"
+           {{enable-tooltip title=this.title subtitle=this.subtitle placement=this.placement icon=this.icon trigger=this.trigger html=this.html}}>
       </div>
     `);
+  }
+
+  test("it doesn't render when the title is empty", async function (assert) {
+    this.title = '';
+    await renderTooltip();
     await assert.tooltip('.test-container').doesNotExist();
   });
 
   test('it renders the correct title', async function (assert) {
-    this.title = 'title';
-    await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title}}>
-      </div>
-    `);
+    await renderTooltip();
     await assert.tooltip('.test-container').hasTitle(this.title);
   });
 
   module('icon attribute', () => {
     test('it renders the correct icon', async function (assert) {
       this.icon = 'far fa-wine-glass-alt';
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title icon=this.icon}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').hasIcon(this.icon);
     });
 
-    test("it doesn't renders the icon", async function (assert) {
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title}}>
-      </div>
-    `);
+    test("it doesn't render the icon", async function (assert) {
+      await renderTooltip();
       await assert.tooltip('.test-container').doesNotHaveIcon();
     });
   });
@@ -57,20 +48,12 @@ module('Integration | Component | modifiers/enable-tooltip', function (hooks) {
   module('subtitle attribute', () => {
     test('it renders the correct subtitle', async function (assert) {
       this.subtitle = 'subtitle';
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title subtitle=this.subtitle}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').hasSubtitle(this.subtitle);
     });
 
     test("it doesn't render the subtitle", async function (assert) {
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').doesNotHaveSubtitle();
     });
   });
@@ -79,11 +62,7 @@ module('Integration | Component | modifiers/enable-tooltip', function (hooks) {
     PLACEMENTS.forEach((placement) => {
       test(`it renders the correct placement ${placement}`, async function (assert) {
         this.placement = placement;
-        await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title placement=this.placement}}>
-      </div>
-    `);
+        await renderTooltip();
         await assert.tooltip('.test-container').hasPlacement(placement || 'bottom');
       });
     });
@@ -91,63 +70,39 @@ module('Integration | Component | modifiers/enable-tooltip', function (hooks) {
 
   module('trigger attribute', () => {
     test('it renders when hovering and focusing the element with undefined trigger', async function (assert) {
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').exists();
       await assert.tooltip('.test-container').exists('focusin');
     });
 
     test('it renders when hovering the element', async function (assert) {
       this.trigger = 'hover';
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title trigger=this.trigger}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').exists();
     });
 
     test('it renders when focusing the element', async function (assert) {
       this.trigger = 'focus';
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title trigger=this.trigger}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').exists('focusin');
     });
   });
 
   module('html attribute', () => {
-    test("it doesn't renders the html safe by default", async function (assert) {
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title}}>
-      </div>
-    `);
+    test("it doesn't render the html safe by default", async function (assert) {
+      await renderTooltip();
       await assert.tooltip('.test-container').isNotHtmlSafe();
     });
 
     test('it renders the html safe when the value is true', async function (assert) {
       this.html = true;
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title html=this.html}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').isHtmlSafe();
     });
 
-    test("it doesn't renders the html safe when the value is false", async function (assert) {
+    test("it doesn't render the html safe when the value is false", async function (assert) {
       this.html = false;
-      await render(hbs`
-      <div class="test-container" style="height: 20px; width: 40px"
-           {{enable-tooltip title=this.title html=this.html}}>
-      </div>
-    `);
+      await renderTooltip();
       await assert.tooltip('.test-container').isNotHtmlSafe();
     });
   });
