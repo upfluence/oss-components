bd38a1eb8715e9c862f83f690ffd85de3e2e5e19
Julien Vannier <julien.vannier66@gmail.com>
2023-06-05 09:53:24 +0200

    PR feedback

--- addon/mixins/upf-table-search.js
+++ addon/mixins/upf-table-search.js
@@ -23,7 +23,7 @@ export default Mixin.create({
         }
 
         return collection.filter((item) => {
-          let itemName = item.get(this.get('searchAttribute') ?? '').toLowerCase();
+          let itemName = (item.get(this.get('searchAttribute')) ?? '').toLowerCase();
 
           let i;
 
