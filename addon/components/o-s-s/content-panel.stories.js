681b4aaccb9a8133ee06bdeb31a7feb9e31dfac7
Antoine Prentout <antoine.prentout@upfluence.com>
2023-06-08 16:27:14 +0200

    Fix: Pr feedback remove fr-row fx-1 default class

--- addon/components/o-s-s/content-panel.stories.js
+++ addon/components/o-s-s/content-panel.stories.js
@@ -18,11 +18,9 @@ const defaultArgs = {};
 const BasicUsageTemplate = (args) => ({
   template: hbs`
     <div class="font-color-black">
-      <OSS::ContentPanel >
-        <div class="fx-col fx-1">
-          <span>Content</span>
-          <span>Goes here</span>
-        </div>
+      <OSS::ContentPanel class="fx-col fx-1">
+        <span>Content</span>
+        <span>Goes here</span>
       </OSS::ContentPanel>
     </div>
   `,
