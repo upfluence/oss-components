ecf470b9ba9a4a10b9b7ac255961fd37da8fd142
Antoine Prentout <antoine.prentout@upfluence.com>
2023-06-29 16:04:48 +0200

    Add: HomeAction on OSS::Layout::Sidebar component

--- addon/components/o-s-s/layout/sidebar.ts
+++ addon/components/o-s-s/layout/sidebar.ts
@@ -1,7 +1,14 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 interface OSSLayoutSidebarArgs {
   logo: string;
+  homeAction?(): void;
 }
 
-export default class OSSLayoutSidebar extends Component<OSSLayoutSidebarArgs> {}
+export default class OSSLayoutSidebar extends Component<OSSLayoutSidebarArgs> {
+  @action
+  onHomeAction(): void {
+    return this.args.homeAction?.();
+  }
+}
