b5ceee7733bf37887179244fbc640cc3fca7ec2b
Julien Vannier <julien.vannier66@gmail.com>
2023-08-30 15:21:49 +0200

    Update format-numeric helper

---
+++ addon/helpers/format-numeric.ts
@@ -0,0 +1,14 @@
+import Helper from '@ember/component/helper';
+
+export function formatNumericHelper(params: any[]) {
+  let [number] = params;
+  let formatter = Intl.NumberFormat(['en-EN', 'fr-FR'], {
+    style: 'decimal',
+    minimumFractionDigits: 0 // show decimals only if there are ones
+  });
+
+  if (isNaN(parseInt(number))) return number;
+  return formatter.format(number);
+}
+
+export default Helper.helper(formatNumericHelper);
--- app/helpers/format-numeric.js
+++ app/helpers/format-numeric.js
@@ -1,14 +1,1 @@
-import Helper from '@ember/component/helper';
-
-export function formatNumericHelper(params) {
-  let [number] = params;
-  let formatter = Intl.NumberFormat(['en-EN', 'fr-FR'], {
-    style: 'decimal',
-    minimumFractionDigits: 0 // show decimals only if there are ones
-  });
-
-  if (isNaN(parseInt(number))) return number;
-  return formatter.format(number);
-}
-
-export default Helper.helper(formatNumericHelper);
+export { default, formatNumericHelper } from '@upfluence/oss-components/helpers/format-numeric';
