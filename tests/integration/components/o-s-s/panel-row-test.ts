f3357316f60770b92eaf18f794c9cc1457029ff6
Owen Coogan <owencoogan01@gmail.com>
2023-09-04 18:22:08 +0200

    chore: added panel row tests

--- tests/integration/components/o-s-s/panel-row-test.ts
+++ tests/integration/components/o-s-s/panel-row-test.ts
@@ -7,8 +7,11 @@ module('Integration | Component | o-s-s/panel-row', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<OSS::Panel::Row />`);
+    await render(hbs`<OSS::Panel::Row @icon={{"fa-cog"}} @label={{'Your Label'l}} />`);
 
-    assert.dom('.oss-panel-row').exists();
+    assert.dom('.oss-panel-content--row').exists();
+    assert.dom('.far').exists();
+    assert.dom('p').hasText('Your Label');
+    assert.dom('.far').exists();
   });
 });
