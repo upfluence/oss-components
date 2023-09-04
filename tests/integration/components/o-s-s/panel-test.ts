b78b4530802e202a11d1804beaf2f25b9705e25f
Owen Coogan <owencoogan01@gmail.com>
2023-09-04 18:15:33 +0200

    chore: added panel tests

--- tests/integration/components/o-s-s/panel-test.ts
+++ tests/integration/components/o-s-s/panel-test.ts
@@ -7,8 +7,36 @@ module('Integration | Component | o-s-s/panel', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<OSS::Panel />`);
+    await render(hbs`
+      <OSS::Panel>
+        <:content>Content</:content>
+      </OSS::Panel>
+    `);
 
-    assert.dom('.oss-panel').exists();
+    assert.dom('.oss-panel--separator').doesNotExist('Separator should not be visible when only "content" is present');
+    assert.dom('.oss-panel--content').hasText('Content', 'Content should be visible');
+    assert.dom('.oss-panel--footer').doesNotExist('Footer should not be visible');
+
+    await render(hbs`
+      <OSS::Panel>
+        <:content>Content</:content>
+        <:footer>Footer</:footer>
+      </OSS::Panel>
+    `);
+
+    assert
+      .dom('.oss-panel--separator')
+      .exists('Separator should be visible when both "content" and "footer" are present');
+    assert.dom('.oss-panel--content').hasText('Content', 'Content should be visible');
+    assert.dom('.oss-panel--footer').hasText('Footer', 'Footer should be visible');
+
+    await render(hbs`
+      <OSS::Panel>
+        <:footer>Footer</:footer>
+      </OSS::Panel>
+    `);
+
+    assert.dom('.oss-panel--separator').doesNotExist('Separator should not be visible when only "footer" is present');
+    assert.dom('.oss-panel--footer').hasText('Footer', 'Footer should be visible');
   });
 });
