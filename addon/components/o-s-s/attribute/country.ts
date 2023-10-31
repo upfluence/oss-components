f3bc1f52d85d3272cd518b8224f4ceee8e73b779
Julien Vannier <julien.vannier66@gmail.com>
2023-10-31 14:56:18 +0100

    OSS::Attribute::Text update

--- addon/components/o-s-s/attribute/country.ts
+++ addon/components/o-s-s/attribute/country.ts
@@ -1,11 +1,14 @@
 import Component from '@glimmer/component';
 import { CountryData, countries } from '@upfluence/oss-components/utils/country-codes';
+import { tracked } from '@glimmer/tracking';
 
 interface OSSAttributeCountryArgs {
   countryCode?: string;
 }
 
 export default class OSSAttributeCountry extends Component<OSSAttributeCountryArgs> {
+  @tracked displayCopyBtn: boolean = false;
+
   private countryDictionnary: CountryData[] = countries;
 
   get countryName(): string {
