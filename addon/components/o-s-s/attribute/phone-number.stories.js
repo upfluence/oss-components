4c7245d53f05b15f0daadd9bfd6b1993418e37bd
MaximilienBrunelle <maximilien.brunelle@gmail.com>
2023-11-09 14:11:24 +0100

    Adds OSS::Attribute::PhoneNumber along with documentation and tests

---
+++ addon/components/o-s-s/attribute/phone-number.stories.js
@@ -0,0 +1,54 @@
+import hbs from 'htmlbars-inline-precompile';
+
+export default {
+  title: 'Components/OSS::Attribute::PhoneNumber',
+  component: 'phone-number',
+  argTypes: {
+    countryCode: {
+      description:
+        'The country-code in alpha2 format that will be used to display the country flag if it matches the dictionary.',
+      table: {
+        defaultValue: { summary: 'undefined' }
+      },
+      control: { type: 'text' }
+    },
+    prefix: {
+      description: 'The phone number prefix to be displayed with a space before the number. (e.g. "+33")',
+      table: {
+        defaultValue: { summary: 'undefined' }
+      },
+      control: { type: 'text' }
+    },
+    number: {
+      description: 'The phone number that will be displayed',
+      table: {
+        defaultValue: { summary: 'undefined' }
+      },
+      control: { type: 'text' }
+    }
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Member of the Attribute displays. The OSS::Attribute::PhoneNumber displays a phone number and a country flag if the country-code is provided.'
+      }
+    }
+  }
+};
+
+const defaultArgs = {
+  countryCode: 'FR'
+};
+
+const Template = (args) => ({
+  template: hbs`
+    <div style="padding: 12px; background: white">
+      <OSS::Attribute::PhoneNumber @countryCode={{this.countryCode}} />
+    </div>
+  `,
+  context: args
+});
+
+export const Default = Template.bind({});
+Default.args = defaultArgs;
---
+++ tests/integration/components/o-s-s/attribute/phone-number-test.ts
@@ -0,0 +1,46 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | o-s-s/attribute/phone-number', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<OSS::Attribute::PhoneNumber />`);
+
+    assert.dom('[data-control-name="attribute-phone-number"]').exists();
+  });
+
+  test('If the number is passed, it is displayed', async function (assert) {
+    await render(hbs`<OSS::Attribute::PhoneNumber @number="+1 212 666 666" />`);
+
+    assert.dom('.oss-attribute__value').hasText('+1 212 666 666');
+  });
+
+  test('If the number is undefined, a dash is displayed', async function (assert) {
+    await render(hbs`<OSS::Attribute::PhoneNumber @prefix="+44" @number="" />`);
+
+    assert.dom('.oss-attribute__value').hasText('-');
+  });
+
+  test('If the prefix is passed with a number, they are both displayed', async function (assert) {
+    await render(hbs`<OSS::Attribute::PhoneNumber @prefix="+44" @number="123 123" />`);
+
+    assert.dom('.oss-attribute__value').hasText('+44 123 123');
+  });
+
+  test('If the countryCode matches one in the dictionary, a flag is displayed', async function (assert) {
+    await render(hbs`<OSS::Attribute::PhoneNumber @countryCode="US" @prefix="+1" @number="123 123" />`);
+
+    assert.dom('.oss-attribute__value').hasText('+1 123 123');
+    assert.dom('.fflag-US').exists();
+  });
+
+  test("If the countryCode doesn't match one in the dictionary, no flag is displayed", async function (assert) {
+    await render(hbs`<OSS::Attribute::PhoneNumber @countryCode="random" @prefix="+1" @number="123 123" />`);
+
+    assert.dom('.oss-attribute__value').hasText('+1 123 123');
+    assert.dom('.fflag-US').doesNotExist();
+  });
+});
