6c4fe614c3b799dd35ad0ef3be01989c288aa5d4
MaximilienBrunelle <maximilien.brunelle@gmail.com>
2023-11-10 11:47:48 +0100

    Adds OSS::Attribute::TagArray component along with tests and documentation

---
+++ addon/components/o-s-s/attribute/tag-array.stories.js
@@ -0,0 +1,54 @@
+import hbs from 'htmlbars-inline-precompile';
+
+export default {
+  title: 'Components/OSS::Attribute::TagArray',
+  component: 'tag-array',
+  argTypes: {
+    label: {
+      description: 'The label of the component.',
+      table: {
+        type: {
+          summary: 'string'
+        },
+        defaultValue: { summary: 'undefined' }
+      },
+      control: { type: 'text' },
+      type: { required: true }
+    },
+    tags: {
+      description: 'An array of tags which will be displayed next to the label.',
+      table: {
+        type: {
+          summary: 'string[]'
+        },
+        defaultValue: { summary: '[]' }
+      },
+      control: { type: 'array' }
+    }
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Member of the Attribute displays. The OSS::Attribute::TagArray displays all the tags that are provided in the @tags parameter.'
+      }
+    }
+  }
+};
+
+const defaultArgs = {
+  label: 'Fruits',
+  tags: ['watermelon', 'vodkamelon', 'whiskeymelon', 'tequilamelon']
+};
+
+const Template = (args) => ({
+  template: hbs`
+    <div style="padding: 12px; background: white">
+      <OSS::Attribute::TagArray @label={{this.label}} @tags={{this.tags}} />
+    </div>
+  `,
+  context: args
+});
+
+export const Default = Template.bind({});
+Default.args = defaultArgs;
