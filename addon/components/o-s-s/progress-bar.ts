6fe1ee48e04089b0bb943f73043743a68f24aded
Owen Coogan <owencoogan01@gmail.com>
2023-11-22 15:30:21 +0100

    feat: added OSS::ProgressBar component

---
+++ addon/components/o-s-s/progress-bar.ts
@@ -0,0 +1,38 @@
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+enum ProgressBarSkins {
+  'primary',
+  'warning',
+  'success'
+}
+
+interface OSSProgressBarArgs {
+  value: number;
+  label?: string;
+  valueIsVisible?: boolean;
+  skin?: ProgressBarSkins;
+}
+
+export default class OSSProgressBar extends Component<OSSProgressBarArgs> {
+  constructor(owner: unknown, args: OSSProgressBarArgs) {
+    super(owner, args);
+
+    assert(
+      '[component][OSS::ProgressBar] You must pass a numbered value between 0 and 100',
+      0 <= args.value && args.value <= 100
+    );
+  }
+
+  get skin() {
+    return this.args.skin || 'primary';
+  }
+
+  get computedStyles() {
+    return 'oss-progress-bar--inner--' + this.skin;
+  }
+
+  get progressBarInnerStyle() {
+    return `width: ${this.args.value + '%'}`;
+  }
+}
