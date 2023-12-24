0c6207985977735f08507aa156bb3bcece2eebd5
Philippe Ndiaye <phndiaye@gmail.com>
2023-12-24 22:21:56 +0100

    Typesafety for fa-icon-value helper and OSS::Button (first component, woop woop)

--- addon/helpers/fa-icon-value.ts
+++ addon/helpers/fa-icon-value.ts
@@ -2,12 +2,26 @@ import Helper from '@ember/component/helper';
 import { assert } from '@ember/debug';
 import { getIconValue } from '../utils/icon-details';
 
-export default class extends Helper {
+type PositionalArgs = string[];
+interface FaIconValueHelperSignature {
+  Args: {
+    Positional: PositionalArgs;
+  };
+  Return: string;
+}
+
+export default class FaIconValueHelper extends Helper<FaIconValueHelperSignature> {
   compute(args: any[]) {
     const faIconString = args[0];
 
     assert('[helper][OSS::fa-icon-value] faIconString argument is mandatory', typeof faIconString === 'string');
 
     return getIconValue(faIconString);
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'fa-icon-value': typeof FaIconValueHelper;
   }
 }
