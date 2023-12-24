92c836603ca379be8a474811e1d979c77be5061f
Philippe Ndiaye <phndiaye@gmail.com>
2023-12-24 19:53:11 +0100

    helpers/fa-icon-style: provide signature

--- addon/helpers/fa-icon-style.ts
+++ addon/helpers/fa-icon-style.ts
@@ -1,13 +1,28 @@
 import Helper from '@ember/component/helper';
 import { assert } from '@ember/debug';
-import { getIconStyle } from '../utils/icon-details';
+import { getIconStyle, type IconStyle } from '../utils/icon-details';
 
-export default class extends Helper {
-  compute(args: any[]) {
+type PositionalArgs = Array<string>;
+
+export interface FaIconStyleSignature {
+  Args: {
+    Positional: PositionalArgs;
+  };
+  Return: IconStyle;
+}
+
+export default class FaIconStyleHelper extends Helper<FaIconStyleSignature> {
+  compute(args: PositionalArgs): IconStyle {
     const faIconString = args[0];
 
     assert('[helper][OSS::fa-icon-style] faIconString argument is mandatory', typeof faIconString === 'string');
 
     return getIconStyle(faIconString);
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'fa-icon-style': typeof FaIconStyleHelper;
   }
 }
