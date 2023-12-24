a8d474809b67a4a81e5e93d63db9dd76f9961a97
Philippe Ndiaye <phndiaye@gmail.com>
2023-12-24 17:33:39 +0100

    Bump ember-auto-import & ember-cli-typescript (leading to other various updates)

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,11 @@
 // Types for compiled templates
-declare module 'upfluence-oss-components/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile';
+declare module '@upfluence/oss-components/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
+
   const tmpl: TemplateFactory;
   export default tmpl;
+}
+
+declare module 'ember-intl/test-support' {
+  function setupIntl(...args: unknown[]): unknown;
 }
