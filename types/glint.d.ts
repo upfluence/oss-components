b9fab5e34402688e5dcbd62e7d140cc1730e290f
Philippe Ndiaye <phndiaye@gmail.com>
2023-12-24 22:21:18 +0100

    Get types for ember-intl t helper & @ember/render-modifiers

--- types/glint.d.ts
+++ types/glint.d.ts
@@ -1,1 +1,11 @@
 import '@glint/environment-ember-loose';
+import '@gavant/glint-template-types/types/ember-intl/helpers/t';
+
+import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
+import TranslationHelper from 'ember-intl/helpers/t';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends RenderModifiersRegistry {
+    t: typeof TranslationHelper;
+  }
+}
