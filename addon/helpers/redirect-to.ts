38ccece45a4ef072b418bb690bbb6fbcce14c637
Philippe Ndiaye <phndiaye@gmail.com>
2023-12-27 14:53:07 +0100

    a bit more typing and fix some linting stuffs

--- addon/helpers/redirect-to.ts
+++ addon/helpers/redirect-to.ts
@@ -6,20 +6,39 @@ type RedirectToHelperArgs = {
   target?: string;
 };
 
-const REDIRECTION_TARGETS: string[] = ['_blank', '_self', '_parent', '_top', 'framename'];
+enum Target {
+  '_blank',
+  '_self',
+  '_parent',
+  '_top',
+  'framename'
+}
 
-export default class extends Helper {
+interface RedirectToHelperSignature {
+  Args: {
+    Named: RedirectToHelperArgs;
+  };
+  Return: () => void;
+}
+
+export default class RedirectToHelper extends Helper<RedirectToHelperSignature> {
   compute(_: any[], named: RedirectToHelperArgs) {
     const { url, target } = named;
 
     assert('[helper][OSS::redirect-to] url argument is mandatory.', typeof url === 'string');
     assert(
       '[helper][OSS::redirect-to] the target argument must be a valid one: https://www.w3schools.com/tags/att_a_target.asp.',
-      target ? REDIRECTION_TARGETS.includes(target) : true
+      target ? Object.keys(Target).includes(target) : true
     );
 
     return () => {
       window.open(url, target ?? '_self');
     };
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'redirect-to': typeof RedirectToHelper;
   }
 }
