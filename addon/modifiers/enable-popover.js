c04eb24ad02b4e7af9af7c357db1f26cd4d97213
Owen Coogan <owencoogan01@gmail.com>
2024-05-15 15:58:18 +0200

    wip: getting rid of jquery popover

--- addon/modifiers/enable-popover.js
+++ addon/modifiers/enable-popover.js
@@ -1,5 +1,4 @@
 import { setModifierManager, capabilities } from '@ember/modifier';
-import jQuery from 'jquery';
 
 export default setModifierManager(
   () => ({
@@ -8,20 +7,65 @@ export default setModifierManager(
     createModifier() {
       return {
         element: null,
-        handler: null
+        handleMouseEnter: null,
+        handleMouseLeave: null
       };
     },
 
-    installModifier(state, element, { positional: [trigger] }) {
+    installModifier(state, element, { positional: [trigger = 'hover'] }) {
       state.element = element;
-      trigger ??= 'hover';
 
-      jQuery(element).popover({ trigger });
+      const handleMouseEnter = () => {
+        const popover = document.createElement('div');
+        popover.textContent = 'azeazeazeaze new';
+        popover.classList.add('oss--popover');
+        popover.setAttribute('role', 'tooltip');
+
+        if (element.getAttribute('data-title')) {
+          const titleNode = document.createTextNode(element.getAttribute('data-title'));
+          const titleDiv = document.createElement('div');
+          titleDiv.appendChild(titleNode);
+          popover.appendChild(titleDiv);
+        }
+        if (element.getAttribute('data-content')) {
+          const contentNode = document.createTextNode(element.getAttribute('data-content'));
+          const contentDiv = document.createElement('div');
+          contentDiv.appendChild(contentNode);
+          popover.appendChild(contentDiv);
+        }
+
+        element.appendChild(popover);
+
+        state.handleMouseLeave = () => {
+          popover.remove();
+          element.removeEventListener('mouseleave', state.handleMouseLeave);
+        };
+
+        element.addEventListener('mouseleave', state.handleMouseLeave);
+      };
+
+      state.handleMouseEnter = handleMouseEnter;
+      element.addEventListener(trigger === 'hover' ? 'mouseenter' : trigger, handleMouseEnter);
     },
 
-    destroyModifier() {
-      // We don't need to do anything here, but a function
-      // still has to be here so we'll leave it blank.
+    updateModifier(state, { positional: [trigger = 'hover'] }) {
+      if (state.handleMouseEnter) {
+        state.element.removeEventListener(trigger === 'hover' ? 'mouseenter' : trigger, state.handleMouseEnter);
+        state.handleMouseEnter = () => {
+          state.element.addEventListener(trigger === 'hover' ? 'mouseenter' : trigger, state.handleMouseEnter);
+        };
+        state.handleMouseEnter();
+      }
+    },
+
+    destroyModifier(state) {
+      if (state.handleMouseEnter) {
+        state.element.removeEventListener('mouseleave', state.handleMouseLeave);
+        state.element.removeEventListener(
+          state.trigger === 'hover' ? 'mouseenter' : state.trigger,
+          state.handleMouseEnter
+        );
+      }
     }
   }),
   class EnablePopoverModifierManager {}
