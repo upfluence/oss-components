f12b4e0458f3c610d9873ff244af15be0aac3321
Owen Coogan <owencoogan01@gmail.com>
2024-05-16 15:59:47 +0200

    wip: popover cleanup

--- addon/modifiers/enable-popover.js
+++ addon/modifiers/enable-popover.js
@@ -17,20 +17,22 @@ export default setModifierManager(
 
       const handleMouseEnter = () => {
         const popover = document.createElement('div');
-        popover.classList.add('oss--popover');
+        popover.classList.add('oss--popover', 'fade-in');
         popover.setAttribute('role', 'tooltip');
-
         const placement = element.getAttribute('data-placement') || 'top';
+        popover.classList.add(placement);
 
         if (element.getAttribute('data-title')) {
           const titleNode = document.createTextNode(element.getAttribute('data-title'));
           const titleDiv = document.createElement('div');
+          titleDiv.classList.add('font-weight-semibold', 'font-size-md', 'font-color-gray-900');
           titleDiv.appendChild(titleNode);
           popover.appendChild(titleDiv);
         }
         if (element.getAttribute('data-content')) {
           const contentNode = document.createTextNode(element.getAttribute('data-content'));
           const contentDiv = document.createElement('div');
+          contentDiv.classList.add('font-color-gray-500');
           contentDiv.appendChild(contentNode);
           popover.appendChild(contentDiv);
         }
@@ -40,30 +42,7 @@ export default setModifierManager(
         const parentRect = element.getBoundingClientRect();
         const popoverRect = popover.getBoundingClientRect();
 
-        let left, top;
-
-        switch (placement) {
-          case 'top':
-            top = parentRect.top - popoverRect.height;
-            left = parentRect.left + parentRect.width / 2 - popoverRect.width / 2;
-            break;
-          case 'bottom':
-            top = parentRect.bottom;
-            left = parentRect.left + parentRect.width / 2 - popoverRect.width / 2;
-            break;
-          case 'left':
-            top = parentRect.top + parentRect.height / 2 - popoverRect.height / 2;
-            left = parentRect.left - popoverRect.width;
-            break;
-          case 'right':
-            top = parentRect.top + parentRect.height / 2 - popoverRect.height / 2;
-            left = parentRect.right;
-            break;
-          default:
-            top = parentRect.top - popoverRect.height;
-            left = parentRect.left + parentRect.width / 2 - popoverRect.width / 2;
-            break;
-        }
+        const { left, top } = this.calculatePosition(parentRect, popoverRect, placement);
 
         popover.style.left = left + 'px';
         popover.style.top = top + 'px';
@@ -98,6 +77,35 @@ export default setModifierManager(
           state.handleMouseEnter
         );
       }
+    },
+
+    calculatePosition(parentRect, popoverRect, placement) {
+      let left, top;
+
+      switch (placement) {
+        case 'top':
+          top = parentRect.top - popoverRect.height - 12;
+          left = parentRect.left + parentRect.width / 2 - popoverRect.width / 2;
+          break;
+        case 'bottom':
+          top = parentRect.bottom + 12;
+          left = parentRect.left + parentRect.width / 2 - popoverRect.width / 2;
+          break;
+        case 'left':
+          top = parentRect.top + parentRect.height / 2 - popoverRect.height / 2;
+          left = parentRect.left - popoverRect.width - 12;
+          break;
+        case 'right':
+          top = parentRect.top + parentRect.height / 2 - popoverRect.height / 2;
+          left = parentRect.right + 12;
+          break;
+        default:
+          top = parentRect.top - popoverRect.height;
+          left = parentRect.left + parentRect.width / 2 - popoverRect.width / 2;
+          break;
+      }
+
+      return { left, top };
     }
   }),
   class EnablePopoverModifierManager {}
