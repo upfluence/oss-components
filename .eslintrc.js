399d379e81c4ab1a894b72e33551d00b2402373f
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2024-07-19 20:47:07 +0200

    Remove upf-table component and related CSS

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,13 +19,11 @@ module.exports = {
     'no-multiple-empty-lines': [2, { max: 1 }],
 
     'ember/no-jquery': 'off',
-    'ember/no-observers': 'off',
     'ember/no-get': 'off',
     'ember/avoid-leaking-state-in-ember-objects': 'off',
     'ember/no-classic-classes': 'off',
     'ember/no-classic-components': 'off',
     'ember/require-tagless-components': 'off',
-    'ember/no-actions-hash': 'off',
     'ember/no-component-lifecycle-hooks': 'off'
   },
   overrides: [
