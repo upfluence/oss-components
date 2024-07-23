871805a2a18250ba4f6b31a4d80a2f7d090cadad
Owen Coogan <owencoogan01@gmail.com>
2024-07-23 16:23:24 +0200

    feat: added optionnal input number & all required tests for the component

--- addon/components/o-s-s/slider.ts
+++ addon/components/o-s-s/slider.ts
@@ -2,15 +2,12 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { guidFor } from '@ember/object/internals';
-import type { Toggle } from './toggle-buttons';
 
 interface SliderComponentArgs {
   value: number;
-  label?: string;
-  tooltipLabel?: string;
-  toggles?: Toggle[];
-  onToggleSwitch?: (value: string) => void;
   onChange?: (value: number) => void;
+  displayInputValue?: boolean;
+  unit?: string;
 }
 
 export default class SliderComponent extends Component<SliderComponentArgs> {
@@ -19,17 +16,13 @@ export default class SliderComponent extends Component<SliderComponentArgs> {
     max: 100
   };
 
-  @tracked currentRangeFilter: number;
-  @tracked selectedToggle: Toggle['value'] | undefined;
+  @tracked currentRangeValue: number;
 
   elementId = `slider-${guidFor(this)}`;
 
   constructor(owner: unknown, args: SliderComponentArgs) {
     super(owner, args);
-    this.currentRangeFilter = this.args.value;
-    if (this.args.toggles) {
-      this.selectedToggle = this.args.toggles[0]?.value ?? undefined;
-    }
+    this.currentRangeValue = this.args.value;
   }
 
   @action
@@ -39,50 +32,30 @@ export default class SliderComponent extends Component<SliderComponentArgs> {
   }
 
   @action
-  onSwitch(value: string) {
-    console.log(value);
-    if (this.args.onToggleSwitch) {
-      this.args.onToggleSwitch(value);
-    }
-  }
-
-  @action
   onTextInput(event: InputEvent) {
     let value = (event.target as HTMLInputElement).valueAsNumber;
+    if (isNaN(value)) {
+      value = this.sliderOptions.min;
+    }
     this.updateValue(value);
   }
 
   updateValue(value: number) {
-    this.currentRangeFilter = value;
+    this.currentRangeValue = value;
     this.updateBackgroundSize(value);
-    this.updateTooltipPosition(value);
     if (this.args.onChange) {
       this.args.onChange(value);
     }
   }
+
   updateBackgroundSize(value: number) {
     let percentage = ((value - this.sliderOptions.min) / (this.sliderOptions.max - this.sliderOptions.min)) * 100;
-    const customRangeElement = document.querySelector('.custom-range') as HTMLElement;
+    const customRangeElement = document.querySelector('.oss-slider--custom-range') as HTMLElement;
     customRangeElement.style.setProperty('--range-percentage', `${percentage}%`);
-  }
-
-  updateTooltipPosition(value: number) {
-    const tooltip = document.querySelector('.upf-tooltip') as HTMLElement;
-    if (tooltip) {
-      const slider = document.querySelector('.custom-range')?.getBoundingClientRect();
-      const sliderContainerRect = document.querySelector('.oss-slider--range-container')?.getBoundingClientRect();
-      if (slider && sliderContainerRect) {
-        const handlePosition =
-          ((value - this.sliderOptions.min) / (this.sliderOptions.max - this.sliderOptions.min)) * slider.width;
-        const tooltipLeft = sliderContainerRect.left + handlePosition - tooltip.offsetWidth / 2;
-        tooltip.style.setProperty('--upf-modifier-tooltip-left', tooltipLeft + 'px');
-      }
-    }
   }
 
   @action
   initializeSlider() {
-    this.updateBackgroundSize(this.currentRangeFilter);
-    this.updateTooltipPosition(this.currentRangeFilter);
+    this.updateBackgroundSize(this.currentRangeValue);
   }
 }
--- tests/integration/components/o-s-s/slider-test.ts
+++ tests/integration/components/o-s-s/slider-test.ts
@@ -1,26 +1,139 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { fillIn, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
+
+function getSliderWidth(element: any): string {
+  const computedStyle = getComputedStyle(element);
+  return computedStyle.getPropertyValue('--range-percentage').trim();
+}
 
 module('Integration | Component | o-s-s/slider', function (hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.value = 10;
+    this.displayInputValue = false;
+    this.unit = '%';
+  });
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function (val) { ... });
+    await render(
+      hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+    );
 
-    await render(hbs`<OSS::Slider />`);
+    assert.dom('.oss-slider--range-container').exists();
+  });
 
-    assert.dom().hasText('');
+  test('it renders the slider width to the proper size', async function (assert) {
+    await render(
+      hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+    );
 
-    // Template block usage:
-    await render(hbs`
-      <OSS::Slider>
-        template block text
-      </OSS::Slider>
-    `);
+    assert.strictEqual(getSliderWidth(this.element.querySelector('.oss-slider--custom-range')), '10%');
+  });
 
-    assert.dom().hasText('template block text');
+  test('the tooltip has the proper value', async function (assert) {
+    this.displayInputValue = true;
+    this.onChange = sinon.stub();
+
+    await render(
+      hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+    );
+
+    await fillIn('.oss-slider--number-input', '10');
+    await assert.tooltip('.oss-slider--custom-range').hasTitle('10%');
+  });
+
+  test('the @onChange method is called with the proper value', async function (assert) {
+    this.displayInputValue = true;
+    this.onChange = sinon.stub();
+
+    await render(
+      hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} @onChange={{this.onChange}}/>`
+    );
+
+    await fillIn('.oss-slider--number-input', '10');
+    assert.ok(this.onChange.calledWith(10));
+  });
+
+  module('value number input', function () {
+    test('it renders the number input when @displayInputValue is truthy', async function (assert) {
+      this.displayInputValue = true;
+
+      await render(
+        hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+      );
+
+      assert.dom('.oss-slider--number-input').exists();
+    });
+
+    test('it renders the slider with a value of 0 when @value is not a number', async function (assert) {
+      this.displayInputValue = true;
+
+      await render(
+        hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+      );
+
+      await fillIn('.oss-slider--number-input', '');
+      assert.dom('.oss-slider--number-input').exists().hasText('');
+      assert.strictEqual(getSliderWidth(this.element.querySelector('.oss-slider--custom-range')), '0%');
+    });
+
+    test('it does not render the number input when @displayInputValue is falsy', async function (assert) {
+      this.displayInputValue = false;
+
+      await render(
+        hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+      );
+
+      assert.dom('.oss-slider--number-input').doesNotExist();
+    });
+
+    test('it updates the input slider width when the number input is updated', async function (assert) {
+      this.displayInputValue = true;
+      this.value = 10;
+
+      await render(
+        hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+      );
+
+      assert.dom('.oss-slider--number-input').exists();
+
+      assert.strictEqual(getSliderWidth(this.element.querySelector('.oss-slider--custom-range')), '10%');
+
+      await fillIn('.oss-slider--number-input', '20');
+
+      assert.strictEqual(getSliderWidth(this.element.querySelector('.oss-slider--custom-range')), '20%');
+    });
+  });
+
+  module('Unit container', function () {
+    hooks.beforeEach(function () {
+      this.displayInputValue = false;
+    });
+
+    test('it does not render the unit container if @unit is undefined', async function (assert) {
+      await render(hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{true}} />`);
+
+      assert.dom('.oss-slider--unit-container').doesNotExist();
+    });
+
+    test('it does not render the unit container if @displayInputValue is falsy', async function (assert) {
+      await render(
+        hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+      );
+
+      assert.dom('.oss-slider--unit-container').doesNotExist();
+    });
+
+    test('it renders the unit container with the proper unit when @unit is defined', async function (assert) {
+      this.displayInputValue = true;
+      await render(
+        hbs`<OSS::Slider @value={{this.value}} @displayInputValue={{this.displayInputValue}} @unit={{this.unit}} />`
+      );
+
+      assert.dom('.oss-slider--unit-container').hasText(this.unit);
+    });
   });
 });
