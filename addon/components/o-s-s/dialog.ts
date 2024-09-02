dd3c760209550b75e1d0819c65259b570fc60e20
Julien Vannier <julien.vannier66@gmail.com>
2024-09-02 12:11:34 +0200

    Add error skin for OSS::Dialog

--- addon/components/o-s-s/dialog.ts
+++ addon/components/o-s-s/dialog.ts
@@ -2,11 +2,19 @@ import BaseModal, { type BaseModalArgs } from './private/base-modal';
 import { action } from '@ember/object';
 import { assert } from '@ember/debug';
 
+type Skin = 'alert' | 'error';
+
 export interface OSSDialogArgs extends BaseModalArgs {
   title: string;
+  skin?: Skin;
   mainAction: { label: string; action: () => unknown };
   secondaryAction: { label: string; action: () => unknown };
 }
+
+const BTN_STYLE_MATCHER: Record<Skin, string> = {
+  alert: 'alert',
+  error: 'destructive'
+};
 
 export default class OSSDialog extends BaseModal<OSSDialogArgs> {
   constructor(owner: unknown, args: OSSDialogArgs) {
@@ -18,6 +26,14 @@ export default class OSSDialog extends BaseModal<OSSDialogArgs> {
       '[component][OSS::Dialog] The secondaryAction parameter is mandatory',
       typeof args.secondaryAction === 'object'
     );
+  }
+
+  get skin(): Skin {
+    return this.args.skin ?? 'alert';
+  }
+
+  get skinBtn(): string {
+    return BTN_STYLE_MATCHER[this.skin];
   }
 
   @action
