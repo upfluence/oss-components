68366f99b56c8b1c815e069c0f3bc21ded2925cb
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2024-12-16 22:01:42 +0100

    One hbs linter plugin to extend from for @upfluence

--- linters/handlebars/index.js
+++ linters/handlebars/index.js
@@ -5,5 +5,25 @@ module.exports = {
 
   rules: {
     'u-template-lint/no-bare-button': NoBareHTMLButton
+  },
+
+  configurations: {
+    recommended: {
+      extends: 'recommended',
+      rules: {
+        'require-valid-alt-text': false,
+        'simple-unless': false,
+        'no-curly-component-invocation': false,
+        'no-inline-styles': false,
+        'require-iframe-title': false,
+        'require-input-label': false,
+        'no-down-event-binding': false,
+        'no-invalid-interactive': false,
+        'no-action': false,
+        'no-nested-interactive': false,
+        'no-yield-only': false,
+        'no-bare-strings': false
+      }
+    }
   }
 };
