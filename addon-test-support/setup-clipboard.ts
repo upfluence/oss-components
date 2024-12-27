3a56759a465a6f557098550d68b4d7242bd8a6e6
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2024-12-27 11:16:24 +0100

    update typing for setup-clipboard test-support function

--- addon-test-support/setup-clipboard.ts
+++ addon-test-support/setup-clipboard.ts
@@ -1,13 +1,14 @@
+import type { TestContext } from '@ember/test-helpers';
 import sinon from 'sinon';
 
 export default function setupClipboard(hooks: NestedHooks) {
-  hooks.beforeEach(function () {
+  hooks.beforeEach(function (this: TestContext) {
     this.permissionQueryStub = sinon
       .stub(navigator.permissions, 'query')
       .resolves({ name: 'clipboard-write', state: 'granted' } as PermissionStatus);
   });
 
-  hooks.afterEach(function () {
+  hooks.afterEach(function (this: TestContext) {
     this.permissionQueryStub.restore();
   });
 }
