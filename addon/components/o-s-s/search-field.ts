798df121de62a4312c99a7d43b01032c6da853fd
Olympe Lespagnon <olympe.lespagnon@upfluence.com>
2025-03-26 10:12:51 +0100

    Update searchfild component to use it where needed

--- addon/components/o-s-s/search-field.ts
+++ addon/components/o-s-s/search-field.ts
@@ -1,11 +1,13 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
+import { onlyNumeric } from '@upfluence/oss-components/utils/keyboard';
 import type IntlService from 'ember-intl/services/intl';
 
 interface OSSSearchFieldArgs {
   value: string;
   placeholder?: string;
+  type?: string;
   onChange(value: string): void;
 }
 
@@ -16,13 +18,33 @@ export default class OSSSearchFieldComponent extends Component<OSSSearchFieldArg
     return this.args.placeholder ?? this.intl.t('oss-components.search_field.placeholder');
   }
 
+  get type(): string {
+    return this.args.type ?? 'text';
+  }
+
   @action
-  onClearSearch(): void {
+  onClearSearch(event: Event): void {
+    event.stopPropagation();
     this.args.onChange('');
   }
 
   @action
-  onChange(value: string): void {
-    this.args.onChange(value);
+  onChange(event: Event): void {
+    this.args.onChange((event.target as HTMLInputElement).value);
+  }
+
+  @action
+  focusSearch(element: HTMLElement): void {
+    element.focus();
+  }
+
+  setupOnlyNumericListener(element: HTMLElement): void {
+    const input: HTMLInputElement | null = element.querySelector('input');
+    input?.addEventListener('keydown', onlyNumeric);
+  }
+
+  teardownOnlyNumericListener(element: HTMLElement): void {
+    const input: HTMLInputElement | null = element.querySelector('input');
+    input?.removeEventListener('keydown', onlyNumeric);
   }
 }
