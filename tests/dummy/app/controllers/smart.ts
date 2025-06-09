8f492d5aacbaa36ee298cb04fd49e182994ca9a6
Nathalie <nathalie.arnoux@upfluence.com>
2025-06-09 16:55:14 +0200

    Added new component Smart ToggleButtons

---
+++ tests/dummy/app/controllers/smart.ts
@@ -0,0 +1,24 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class Smart extends Controller {
+  @tracked selectedToggle: string = 'first';
+  @tracked selectedToggleTwo: string = 'second';
+  @tracked toggles: { value: string; label: string }[] = [
+    {
+      value: 'first',
+      label: 'First'
+    },
+    {
+      value: 'second',
+      label: 'Second'
+    }
+  ];
+
+  @action
+  triggerSelection(value: string): void {
+    console.log('selected toggle value : ', value);
+    this.selectedToggle = value;
+  }
+}
