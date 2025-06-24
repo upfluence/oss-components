329f60dd8c00fbcde7cfcd117e54950f47619588
Owen Coogan <owencoogan01@gmail.com>
2025-06-24 11:02:19 +0200

    fix: added story & fixed styling

--- tests/dummy/app/controllers/smart.ts
+++ tests/dummy/app/controllers/smart.ts
@@ -7,30 +7,25 @@ export default class Smart extends Controller {
   @tracked selectedToggleTwo: string = 'second';
   @tracked toggleInputLoadingValue: boolean = false;
   @tracked smartFeedback: boolean = false;
+  @tracked smartFeedbackLoading: boolean = false;
   @tracked contentArray: string[] = [
-    'This is the first content',
-    'This is the second content',
-    'This is the third content',
-    'This is the first content',
-    'This is the second content',
-    'This is the third content',
     'This is the first content',
     'This is the second content',
     'This is the third content'
   ];
   @tracked toggles: { value: string; label: string }[] = [
-    {
-      value: 'first',
-      label: 'First'
-    },
-    {
-      value: 'second',
-      label: 'Second'
-    }
+    { value: 'first', label: 'First' },
+    { value: 'second', label: 'Second' }
   ];
 
   @tracked declare value: string;
   @tracked loading: boolean = false;
+  intervalId?: number;
+
+  constructor() {
+    super(...arguments);
+    this.addContentToFeedbackComponent();
+  }
 
   @action
   triggerSelection(value: string): void {
@@ -53,6 +48,36 @@ export default class Smart extends Controller {
   }
   @action
   toggleSmartFeedbackLoading(): void {
-    this.smartFeedback = !this.smartFeedback;
+    this.smartFeedbackLoading = !this.smartFeedbackLoading;
+  }
+
+  addContentToFeedbackComponent(): void {
+    const wordsToAdd = ['Dynamic word 1', 'Dynamic word 2', 'Dynamic word 3', 'Dynamic word 4', 'Dynamic word 5'];
+
+    let index = 0;
+
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    this.intervalId = window.setInterval(() => {
+      if (index >= wordsToAdd.length) {
+        clearInterval(this.intervalId);
+        return;
+      }
+
+      const wordToAdd = wordsToAdd[index];
+      if (typeof wordToAdd === 'string') {
+        this.contentArray = [...this.contentArray, wordToAdd];
+      }
+      index++;
+    }, 1000);
+  }
+
+  stopAddingContent(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = undefined;
+    }
   }
 }
