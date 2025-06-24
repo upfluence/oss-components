1a37e9ef392f223af547d4b696c80e7b06a9916b
Owen Coogan <owencoogan01@gmail.com>
2025-06-24 11:02:19 +0200

    fix: added story & fixed styling

--- addon/components/o-s-s/smart/feedback.ts
+++ addon/components/o-s-s/smart/feedback.ts
@@ -7,26 +7,10 @@ interface OSSSmartFeedbackArgs {
 }
 
 export default class OSSSmartFeedback extends Component<OSSSmartFeedbackArgs> {
-  constructor(owner: unknown, args: OSSSmartFeedbackArgs) {
-    super(owner, args);
-  }
-
   get currentStateClass(): string | null {
-    console.log(this.args.loading);
     if (this.args.loading === true) return 'oss-smart__feedback__loading';
     if (this.args.loading === false && (this.args.contentString || this.args.contentArray))
       return 'oss-smart__generated';
     return null;
-  }
-
-  get contentArrayWithDelays() {
-    if (!this.args.contentArray) return [];
-
-    return this.args.contentArray.map((item, index) => {
-      return {
-        text: item,
-        delay: `${index * 0.5}s`
-      };
-    });
   }
 }
