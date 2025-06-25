08b4177acf734e2d5ef8267cfa453985cb202465
Owen Coogan <owencoogan01@gmail.com>
2025-06-25 10:42:50 +0200

    fix: added fixes post review

--- tests/integration/components/o-s-s/smart/input-test.ts
+++ tests/integration/components/o-s-s/smart/input-test.ts
@@ -1,54 +1,147 @@
+import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import { render, find, typeIn, triggerEvent } from '@ember/test-helpers';
+import sinon from 'sinon';
 
 module('Integration | Component | o-s-s/smart/input', function (hooks) {
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
-    this.value = '';
-    this.loading = false;
-    this.label = '';
-  });
-
-  test('it renders empty by default', async function (assert) {
+  test('it renders', async function (assert) {
     await render(hbs`<OSS::Smart::Input />`);
-    assert.dom().exists();
+    assert.dom('.upf-input').exists();
   });
 
-  test('it renders placeholder if passed', async function (assert) {
-    await render(hbs`<OSS::Smart::Input @placeholder="Enter email" />`);
-    assert.dom('input').hasAttribute('placeholder', 'Enter email');
+  module('Input Blocks', () => {
+    async function renderComponentWithPrefixSuffix() {
+      await render(hbs`
+        <OSS::Smart::Input>
+          <:prefix>
+            <OSS::Icon @style="solid" @icon="fa-user" />
+          </:prefix>
+          <:input>
+            <Input id="custom-input"/>
+          </:input>
+          <:suffix>
+            <OSS::Icon @style="solid" @icon="fa-times" />
+          </:suffix>
+        </OSS::Smart::Input>`);
+    }
+
+    test('Prefix block renders properly', async function (assert) {
+      await renderComponentWithPrefixSuffix();
+      assert.dom('.fa-user').exists();
+    });
+
+    test('Suffix block renders properly', async function (assert) {
+      await renderComponentWithPrefixSuffix();
+      assert.dom('.fa-times').exists();
+    });
+
+    test('Custom input block overrides default', async function (assert) {
+      await renderComponentWithPrefixSuffix();
+      assert.dom('#custom-input').exists();
+    });
   });
 
-  test('it binds @value to the input', async function (assert) {
-    this.value = 'demo@example.com';
-    await render(hbs`<OSS::Smart::Input @value={{this.value}} />`);
-    assert.dom('input').hasValue('demo@example.com');
+  module('Component Parameters', (hooks) => {
+    let onValueChange: sinon.SinonSpy<any[], any>;
+    hooks.beforeEach(function () {
+      onValueChange = sinon.fake();
+      this.value = 'smartInput';
+      this.placeholder = 'Type here';
+      this.onValueChange = onValueChange;
+    });
+
+    async function renderComponentWithParams() {
+      await render(hbs`
+        <OSS::Smart::Input
+          @value={{this.value}}
+          @onChange={{this.onValueChange}}
+          @placeholder={{this.placeholder}}
+        />
+      `);
+    }
+
+    test('Binds @value correctly', async function (assert) {
+      await renderComponentWithParams();
+      assert.dom('.upf-input').hasValue('smartInput');
+    });
+
+    test('Sets @placeholder correctly', async function (assert) {
+      await renderComponentWithParams();
+      let input = find('.upf-input');
+      assert.strictEqual(input?.getAttribute('placeholder'), 'Type here');
+    });
+
+    test('Calls @onChange on input', async function (assert) {
+      await renderComponentWithParams();
+      let input = find('.upf-input');
+      assert.ok(input, 'Input element should exist');
+      await typeIn(input as Element, 'X');
+      assert.ok(onValueChange.called);
+    });
+
+    test('Calls @onChange on paste event', async function (assert) {
+      this.set('onChange', sinon.stub());
+      await render(hbs`
+        <OSS::Smart::Input
+          data-control-name="smart-input"
+          @onChange={{this.onChange}}
+        />
+      `);
+      assert.ok(this.onChange.notCalled);
+
+      await triggerEvent('.oss-input-container input', 'paste', {
+        clipboardData: { getData: (format: any) => `clip/${format}` }
+      });
+
+      assert.ok(this.onChange.calledWith('clip/Text'));
+    });
   });
 
-  test('it shows animated text when loading is true', async function (assert) {
-    this.placeholder = 'placeholder';
-    this.loading = true;
+  module('Smart Input Specific Features', () => {
+    test('Displays animated placeholder when loading is true', async function (assert) {
+      this.setProperties({ loading: true, placeholder: 'Smart...' });
 
-    await render(hbs`
-      <OSS::Smart::Input
-        @placeholder={{this.placeholder}}
-        @loading={{this.loading}}
-      />
-    `);
+      await render(hbs`
+        <OSS::Smart::Input
+          @loading={{this.loading}}
+          @placeholder={{this.placeholder}}
+        />
+      `);
 
-    assert.dom('.rainbow_text_animated').hasText(this.placeholder);
+      assert.dom('.rainbow_text_animated').hasText('Smart...');
+    });
   });
 
-  test('it shows error message when @errorMessage is passed', async function (assert) {
-    await render(hbs`
-      <OSS::Smart::Input
-        @errorMessage="Something went wrong"
-      />
-    `);
+  module('Feedback and Error States', () => {
+    test('Displays success feedback', async function (assert) {
+      await render(hbs`
+        <OSS::Smart::Input
+          @feedbackMessage={{hash type="success" value="All good!"}}
+        />
+      `);
+      assert.dom('.oss-input-container--success').exists();
+      assert.dom('.font-color-success-500').hasText('All good!');
+    });
 
-    assert.dom('.text-color-error').hasText('Something went wrong');
+    test('Displays error message', async function (assert) {
+      await render(hbs`<OSS::Smart::Input @errorMessage="Oops" />`);
+      assert.dom('.oss-input-container--errored').exists();
+      assert.dom('.text-color-error').hasText('Oops');
+    });
+  });
+
+  module('Extra attributes', () => {
+    test('Custom class is applied', async function (assert) {
+      await render(hbs`<OSS::Smart::Input class="extra-class" />`);
+      assert.dom('.extra-class').exists();
+    });
+
+    test('data-control-name is passed through', async function (assert) {
+      await render(hbs`<OSS::Smart::Input data-control-name="smart-name" />`);
+      assert.dom('.oss-input-container').hasAttribute('data-control-name', 'smart-name');
+    });
   });
 });
