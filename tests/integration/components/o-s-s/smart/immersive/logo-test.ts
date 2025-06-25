aa9ab5da25f589e3ad87e4352caede6a635e7270
Owen Coogan <owencoogan01@gmail.com>
2025-06-25 17:21:55 +0200

    wip: smart logo

--- tests/integration/components/o-s-s/smart/immersive/logo-test.ts
+++ tests/integration/components/o-s-s/smart/immersive/logo-test.ts
@@ -2,25 +2,54 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
 
 module('Integration | Component | o-s-s/smart/immersive/logo', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function (val) { ... });
+  test('it renders with default icon mode', async function (assert) {
+    this.icon = 'fa:star';
+    this.editable = false;
 
-    await render(hbs`<OSS::Smart::Immersive::Logo />`);
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+    />`);
 
-    assert.dom().hasText('');
+    assert.dom('.logo-icon').exists();
+    assert.dom('.logo-icon').hasClass('logo-icon-color_star');
+    assert.dom('i.fad.fa-fa').exists();
+    assert.dom('.edit-overlay').doesNotExist();
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <OSS::Smart::Immersive::Logo>
-        template block text
-      </OSS::Smart::Immersive::Logo>
-    `);
+  test('it renders edit overlay when editable', async function (assert) {
+    this.icon = 'fa:pen';
+    this.editable = true;
+    this.onClick = sinon.stub();
 
-    assert.dom().hasText('template block text');
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+      @onClick={{this.onClick}}
+    />`);
+
+    assert.dom('.edit-overlay').exists();
+    await this.element.querySelector('.edit-overlay').click();
+    assert.ok(this.onClick.calledOnce);
+  });
+
+  test('it renders image mode when url is provided', async function (assert) {
+    this.url = 'https://example.com/image.png';
+    this.editable = false;
+
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @url={{this.url}}
+      @editable={{this.editable}}
+    />`);
+
+    assert.dom('.campaign-image').exists();
+    let style = this.element.querySelector('.campaign-image').style.backgroundImage;
+    assert.ok(style.includes(this.url));
+    assert.dom('.logo-icon').doesNotExist();
   });
 });
