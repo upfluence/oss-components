ed2b26fa66a447eaccdc21fd7809604469afc670
Owen Coogan <owencoogan01@gmail.com>
2025-06-25 09:13:09 +0200

    fix: added fixes post review

--- addon/components/o-s-s/smart/feedback.ts
+++ addon/components/o-s-s/smart/feedback.ts
@@ -8,9 +8,8 @@ interface OSSSmartFeedbackArgs {
 
 export default class OSSSmartFeedback extends Component<OSSSmartFeedbackArgs> {
   get currentStateClass(): string | null {
-    if (this.args.loading === true) return 'oss-smart__feedback__loading';
-    if (this.args.loading === false && (this.args.contentString || this.args.contentArray))
-      return 'oss-smart__generated';
+    if (this.args.loading) return 'oss-smart__feedback__loading';
+    if (this.args.contentString || this.args.contentArray) return 'oss-smart__generated';
     return null;
   }
 }
