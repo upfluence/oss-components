0fd45c7d83b54afa6a8ad86b8abfe1c81f631375
Owen Coogan <owencoogan01@gmail.com>
2025-06-26 14:42:11 +0200

    fix: added test

--- tests/integration/components/o-s-s/smart/immersive/logo-test.ts
+++ tests/integration/components/o-s-s/smart/immersive/logo-test.ts
@@ -7,7 +7,7 @@ import sinon from 'sinon';
 module('Integration | Component | o-s-s/smart/immersive/logo', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders with default icon mode', async function (assert) {
+  test('it renders with icon mode', async function (assert) {
     this.icon = 'fa:star';
     this.editable = false;
 
@@ -20,22 +20,6 @@ module('Integration | Component | o-s-s/smart/immersive/logo', function (hooks) 
     assert.dom('.logo-icon').hasClass('logo-icon-color_star');
     assert.dom('i.fad.fa-fa').exists();
     assert.dom('.edit-overlay').doesNotExist();
-  });
-
-  test('it renders edit overlay when editable', async function (assert) {
-    this.icon = 'fa:pen';
-    this.editable = true;
-    this.onClick = sinon.stub();
-
-    await render(hbs`<OSS::Smart::Immersive::Logo
-      @icon={{this.icon}}
-      @editable={{this.editable}}
-      @onClick={{this.onClick}}
-    />`);
-
-    assert.dom('.edit-overlay').exists();
-    await this.element.querySelector('.edit-overlay').click();
-    assert.ok(this.onClick.calledOnce);
   });
 
   test('it renders image mode when url is provided', async function (assert) {
@@ -51,5 +35,35 @@ module('Integration | Component | o-s-s/smart/immersive/logo', function (hooks) 
     let style = this.element.querySelector('.campaign-image').style.backgroundImage;
     assert.ok(style.includes(this.url));
     assert.dom('.logo-icon').doesNotExist();
+  });
+
+  test('it renders edit overlay when editable', async function (assert) {
+    this.icon = 'fa:pen';
+    this.editable = true;
+    this.onClick = sinon.stub();
+
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+      @onClick={{this.onClick}}
+    />`);
+
+    assert.dom('.edit-overlay').exists();
+  });
+
+  test('onClick is called when the component is clicked', async function (assert) {
+    this.icon = 'fa:pen';
+    this.editable = true;
+    this.onClick = sinon.stub();
+
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+      @onClick={{this.onClick}}
+    />`);
+
+    assert.dom('.edit-overlay').exists();
+    await this.element.querySelector('.edit-overlay').click();
+    assert.ok(this.onClick.calledOnce);
   });
 });
