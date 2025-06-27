50afbffc5c9ed63ee838bf341b9c97ad723f14a5
Owen Coogan <owencoogan01@gmail.com>
2025-06-27 16:26:34 +0200

    fix: added fixes post review

--- addon/components/o-s-s/smart/immersive/logo.ts
+++ addon/components/o-s-s/smart/immersive/logo.ts
@@ -1,7 +1,10 @@
+import { assert } from '@ember/debug';
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { runSmartGradientAnimation } from '@upfluence/oss-components/utils/run-smart-gradient-animation';
+
+const FALLBACK_SVG_URL = '/assets/images/picture-frame.svg';
 
 interface OSSSmartImmersiveLogoArgs {
   icon?: string;
@@ -15,6 +18,24 @@ interface OSSSmartImmersiveLogoArgs {
 export default class OSSSmartImmersiveLogoComponent extends Component<OSSSmartImmersiveLogoArgs> {
   @tracked declare element: HTMLElement;
 
+  constructor(owner: unknown, args: OSSSmartImmersiveLogoArgs) {
+    super(owner, args);
+
+    if (args.icon) {
+      const parts = args.icon.split(':');
+      if (parts) {
+        assert(
+          `Invalid icon format "${args.icon}". Expected format is "iconName:colorName".`,
+          parts.length === 2 &&
+            typeof parts[0] === 'string' &&
+            typeof parts[1] === 'string' &&
+            parts[0].trim().length > 0 &&
+            parts[1].trim().length > 0
+        );
+      }
+    }
+  }
+
   get logoColor(): string | null {
     return this.args.icon ? `logo-icon-color_${this.args.icon.split(':')[1]}` : null;
   }
@@ -24,11 +45,16 @@ export default class OSSSmartImmersiveLogoComponent extends Component<OSSSmartIm
   }
 
   get dynamicImageStyle(): string | null {
-    return this.args.url ? `background-image: url(${this.args.url})` : null;
+    if (this.args.url) {
+      return `background-image: url(${this.args.url})`;
+    } else if (!this.args.icon) {
+      return `background-image: url(${FALLBACK_SVG_URL})`;
+    }
+    return null;
   }
 
   get logoMode(): 'icon' | 'image' {
-    return this.args.url ? 'image' : 'icon';
+    return this.args.url || !this.args.icon ? 'image' : 'icon';
   }
 
   @action
