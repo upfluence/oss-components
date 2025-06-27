bca0998fd997700150d968a99a3d68844559874b
Owen Coogan <owencoogan01@gmail.com>
2025-06-27 16:26:34 +0200

    fix: added fixes post review

--- addon/components/o-s-s/smart/immersive/logo.stories.js
+++ addon/components/o-s-s/smart/immersive/logo.stories.js
@@ -1,17 +1,25 @@
 import { hbs } from 'ember-cli-htmlbars';
 import { action } from '@storybook/addon-actions';
+import { LOGO_COLORS, LOGO_ICONS } from '../../../../../app/utils/logo-config';
 
 export default {
   title: 'Components/Smart::Immersive::Logo',
   component: 'oss-smart-immersive-logo',
   argTypes: {
-    icon: {
-      description: 'Icon name in format "name:color", e.g. "rabbit:orange"',
-      table: {
-        type: { summary: 'string' },
-        defaultValue: { summary: 'undefined' }
-      },
-      control: { type: 'text' }
+    icon: { table: { disable: true } },
+    iconName: {
+      name: 'Icon Name',
+      control: {
+        type: 'select',
+        options: LOGO_ICONS
+      }
+    },
+    iconColor: {
+      name: 'Icon Color',
+      control: {
+        type: 'select',
+        options: LOGO_COLORS
+      }
     },
     url: {
       description: 'URL of the image for image mode',
@@ -62,8 +70,34 @@ export default {
   }
 };
 
-const defaultArgs = {
-  icon: 'rabbit:orange',
+const Template = ({ iconName, iconColor, ...rest }) => {
+  // Compose icon string
+  const icon = iconName && iconColor ? `${iconName}:${iconColor}` : undefined;
+
+  return {
+    template: hbs`
+      <div style="width: 350px; background-color: var(--color-gray-50); padding: var(--spacing-px-24);">
+        <OSS::Smart::Immersive::Logo
+          @icon={{this.icon}}
+          @url={{this.url}}
+          @editable={{this.editable}}
+          @loading={{this.loading}}
+          @oversize={{this.oversize}}
+          @onClick={{this.onClick}}
+        />
+      </div>
+    `,
+    context: {
+      ...rest,
+      icon
+    }
+  };
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  iconName: 'rabbit',
+  iconColor: 'orange',
   url: undefined,
   editable: false,
   loading: false,
@@ -71,45 +105,26 @@ const defaultArgs = {
   onClick: action('onClick')
 };
 
-const Template = (args) => ({
-  template: hbs`
-    <div style="width: 350px; background-color: var(--color-gray-50); padding: var(--spacing-px-24);">
-      <OSS::Smart::Immersive::Logo
-        @icon={{this.icon}}
-        @url={{this.url}}
-        @editable={{this.editable}}
-        @loading={{this.loading}}
-        @oversize={{this.oversize}}
-        @onClick={{this.onClick}}
-      />
-    </div>
-  `,
-  context: args
-});
-
-export const Default = Template.bind({});
-Default.args = defaultArgs;
-
 export const Editable = Template.bind({});
 Editable.args = {
-  ...defaultArgs,
+  ...Default.args,
   editable: true
 };
 
 export const ImageMode = Template.bind({});
 ImageMode.args = {
-  ...defaultArgs,
+  ...Default.args,
   url: 'https://example.com/logo.png'
 };
 
 export const LoadingState = Template.bind({});
 LoadingState.args = {
-  ...defaultArgs,
+  ...Default.args,
   loading: true
 };
 
 export const OversizeState = Template.bind({});
 OversizeState.args = {
-  ...defaultArgs,
+  ...Default.args,
   oversize: true
 };
