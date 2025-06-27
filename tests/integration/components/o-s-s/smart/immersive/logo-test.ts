af9a6c67cdb22553ea5c8f8c9b44d0b09794e801
Owen Coogan <owencoogan01@gmail.com>
2025-06-27 16:26:34 +0200

    fix: added fixes post review

--- tests/integration/components/o-s-s/smart/immersive/logo-test.ts
+++ tests/integration/components/o-s-s/smart/immersive/logo-test.ts
@@ -1,8 +1,9 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, setupOnerror } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
+import { LOGO_COLORS, LOGO_ICONS } from 'dummy/utils/logo-config';
 
 module('Integration | Component | o-s-s/smart/immersive/logo', function (hooks) {
   setupRenderingTest(hooks);
@@ -65,5 +66,89 @@ module('Integration | Component | o-s-s/smart/immersive/logo', function (hooks) 
     assert.dom('.edit-overlay').exists();
     await this.element.querySelector('.edit-overlay').click();
     assert.ok(this.onClick.calledOnce);
+  });
+
+  LOGO_ICONS.forEach((iconName) => {
+    const icon = `${iconName}:orange`;
+    test(`renders icon mode correctly for icon "${icon}"`, async function (assert) {
+      this.icon = icon;
+      this.editable = false;
+
+      await render(hbs`<OSS::Smart::Immersive::Logo
+          @icon={{this.icon}}
+          @editable={{this.editable}}
+        />`);
+
+      assert.dom(`.logo-icon i.fa-${iconName}`).exists();
+    });
+  });
+
+  LOGO_COLORS.forEach((color) => {
+    const icon = `rabbit:${color}`;
+    test(`renders icon mode correctly for color "${color}"`, async function (assert) {
+      this.icon = icon;
+      this.editable = false;
+
+      await render(hbs`<OSS::Smart::Immersive::Logo
+        @icon={{this.icon}}
+        @editable={{this.editable}}
+      />`);
+
+      assert.dom(`.logo-icon`).hasClass(`logo-icon-color_${color}`);
+    });
+  });
+
+  test(`it renders the generating animation when loading is true`, async function (assert) {
+    this.loading = true;
+
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+      @loading={{this.loading}}
+    />`);
+
+    assert.dom('.oss-smart__immersive-icon-container__generating').exists();
+  });
+
+  test(`it renders the generated content animation when loading is false`, async function (assert) {
+    this.loading = true;
+
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+      @loading={{this.loading}}
+    />`);
+
+    this.set('loading', false);
+    assert.dom('.smart-rotating-gradient').exists();
+  });
+
+  test('it throws an error if icon format is invalid', async function (assert) {
+    this.icon = 'invalidFormat';
+
+    setupOnerror((error: Error) => {
+      assert.equal(
+        error.message,
+        `Assertion Failed: Invalid icon format "${this.icon}". Expected format is "iconName:colorName".`
+      );
+    });
+
+    render(hbs`<OSS::Smart::Immersive::Logo
+      @icon={{this.icon}}
+      @editable={{this.editable}}
+    />`);
+  });
+
+  test('it uses the fallback SVG when both icon and url are missing', async function (assert) {
+    this.editable = false;
+
+    await render(hbs`<OSS::Smart::Immersive::Logo
+      @editable={{this.editable}}
+      @onClick={{this.onClick}}
+    />`);
+
+    assert.dom('.campaign-image').exists('Fallback image container is rendered');
+    const style = this.element.querySelector('.campaign-image')?.getAttribute('style');
+    assert.ok(style?.includes('background-image: url(/assets/images/picture-frame.svg)'));
   });
 });
