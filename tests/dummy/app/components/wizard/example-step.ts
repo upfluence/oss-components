62eb8eab8f634ec817670795bc42348083c32b46
Maximilien B. <maximilien.brunelle@gmail.com>
2025-07-08 15:14:53 +0200

    Added: new wizard route in dummy app and a bunch of example components

---
+++ tests/dummy/app/components/wizard/example-step.ts
@@ -0,0 +1,53 @@
+import { action } from '@ember/object';
+import { WizardBaseStep, type WizardBaseStepSignature } from '@upfluence/oss-components/components/wizard/base-step';
+
+interface WizardExampleStepSignature extends WizardBaseStepSignature {}
+
+export default class WizardExampleStep extends WizardBaseStep<WizardExampleStepSignature> {
+  constructor(owner: unknown, args: WizardExampleStepSignature) {
+    super(owner, args);
+    console.log('WizardExampleStep initialized for step:', this.args.step.id);
+    // Ensure the submission method is set
+    if (!this.args.step.validateStep) {
+      this.args.step.validateStep = this.onStepSubmission.bind(this);
+    }
+  }
+
+  @action
+  onStepSubmission(): Promise<boolean> {
+    console.log('onStepSubmission called for step:', this.args.step.id);
+    return new Promise((resolve) => {
+      // Implement the logic for step submission here
+      // For example, this can be bound to the submit button of an OSS::Form component
+      // we would then validate the inputs and resolve true or false based on the validations
+      resolve(true || false);
+    });
+  }
+
+  @action
+  onFirstInsertion(): void {
+    console.log('onFirstInsertion called for step:', this.args.step.id);
+    // This is triggered the first time the step becomes active
+    // You can add any additional logic that should run when the step becomes active
+    // ie. set the loading states on the components
+    // and run the calls to the API to fetch JACE data
+  }
+
+  @action
+  onRevisit(): void {
+    console.log('onRevisit called for step:', this.args.step.id);
+    // This is triggered when the step is revisited
+    // We will use it to rerun validations to display any errors that would be present
+    this.onStepSubmission();
+  }
+
+  override onLeave(): void {
+    // Triggered when this step is no longer active (ie. moved to preview)
+    console.log('onLeave called for step:', this.args.step.id);
+  }
+
+  willDestroy(): void {
+    super.willDestroy();
+    console.log('onDestroy called for step:', this.args.step.id);
+  }
+}
---
+++ tests/dummy/app/controllers/wizard.ts
@@ -0,0 +1,40 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import type WizardManager from '@upfluence/oss-components/services/wizard-manager';
+import type { WizardConfiguration } from '@upfluence/oss-components/services/wizard-manager';
+import WizardExampleStep from '../components/wizard/example-step';
+import WizardExampleStepWithScroll from '../components/wizard/example-step-with-scroll';
+import OSSInputContainer from '@upfluence/oss-components/components/o-s-s/input-container';
+
+const WIZARD_CONFIG: WizardConfiguration = {
+  options: {
+    centerStepsInContainer: true
+  },
+  sections: [
+    {
+      key: 'Section 1',
+      steps: [
+        { key: 'Step 1', componentClass: WizardExampleStepWithScroll },
+        { key: 'Step 2', componentClass: WizardExampleStep },
+        // Usage with a random component that is not extending WizardBaseStep:
+        { key: 'Step 3', componentClass: OSSInputContainer, validateStep: () => Promise.resolve(true) }
+      ]
+    },
+    {
+      key: 'Section 2',
+      steps: [
+        { key: 'Step 4', componentClass: WizardExampleStep },
+        { key: 'Step 5', componentClass: WizardExampleStep }
+      ]
+    }
+  ]
+};
+
+export default class Wizard extends Controller {
+  @service declare wizardManager: WizardManager;
+
+  constructor() {
+    super(...arguments);
+    this.wizardManager.initialize(WIZARD_CONFIG);
+  }
+}
