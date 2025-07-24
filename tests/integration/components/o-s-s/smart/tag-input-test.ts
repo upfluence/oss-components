2963e296a59c57b6055e9af4d8bc72771a1d20f1
Nathalie <nathalie.arnoux@upfluence.com>
2025-07-24 17:49:58 +0200

    Added Smart::Tag & Smart::TagInput components

---
+++ tests/integration/components/o-s-s/smart/tag-input-test.ts
@@ -0,0 +1,52 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, triggerKeyEvent, fillIn } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
+import { setupIntl } from 'ember-intl/test-support';
+
+module('Integration | Component | o-s-s/smart/tag-input', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  hooks.beforeEach(function () {
+    this.onKeydown = sinon.stub();
+  });
+
+  test('it renders', async function (assert) {
+    await render(hbs`<OSS::Smart::TagInput @value="" @onKeydown={{this.onKeydown}} />`);
+    assert.dom('.tag-input-container').exists();
+  });
+
+  test('it renders the empty state with placeholder', async function (assert) {
+    await render(hbs`<OSS::Smart::TagInput @value="" @onKeydown={{this.onKeydown}} />`);
+    assert
+      .dom('.tag-input-empty-state')
+      .hasText(this.intl.t('oss-components.smart.tag_input.placeholder'), 'Placeholder text is rendered');
+  });
+
+  test('it calls @onKeydown when pressing Enter', async function (assert) {
+    await render(hbs`<OSS::Smart::TagInput @value="" @onKeydown={{this.onKeydown}} />`);
+    await fillIn('[data-control-name="tag-input"]', 'foo');
+    await triggerKeyEvent('[data-control-name="tag-input"]', 'keydown', 'Enter');
+    assert.true(this.onKeydown.calledOnce, 'onKeydown is called once');
+  });
+
+  test('input is cleared after save', async function (assert) {
+    await render(hbs`<OSS::Smart::TagInput @value="" @onKeydown={{this.onKeydown}} />`);
+    await fillIn('[data-control-name="tag-input"]', 'foo');
+    assert.dom('[data-control-name="tag-input"]').hasValue('foo', 'Input has value "foo"');
+    await triggerKeyEvent('[data-control-name="tag-input"]', 'keydown', 'Enter');
+    assert.dom('[data-control-name="tag-input"]').hasValue('', 'Input is cleared after save');
+  });
+
+  test('it renders the animated overlay when loading', async function (assert) {
+    await render(hbs`<OSS::Smart::TagInput @loading={{true}} @value="" @onKeydown={{this.onKeydown}} />`);
+    assert.dom('.animated-overlay').exists('Animated overlay is shown');
+  });
+
+  test('it applies the error class when @hasError is true', async function (assert) {
+    await render(hbs`<OSS::Smart::TagInput @hasError={{true}} @value="" @onKeydown={{this.onKeydown}} />`);
+    assert.dom('.tag-input--error').exists('Error class is present');
+  });
+});
