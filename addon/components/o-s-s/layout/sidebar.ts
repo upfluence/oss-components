8fa1ad679db8d1a4af98f917b509c245898f05ba
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2025-09-01 17:10:40 +0200

    oss/layout/sidebar: add support for expandable/collapsible sidebar

--- addon/components/o-s-s/layout/sidebar.ts
+++ addon/components/o-s-s/layout/sidebar.ts
@@ -1,14 +1,52 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { scheduleOnce } from '@ember/runloop';
+
+import UPFLocalStorage from '@upfluence/oss-components/utils/upf-local-storage';
 
 export type HomeParameters = {
   logo: string;
   url: string;
 };
 
+const SIDEBAR_EXPANDED_STATE = 'oss-layout-sidebar-expanded';
+
 interface OSSLayoutSidebarArgs {
   homeParameters?: HomeParameters;
-  logo: string;
+  logo?: string;
   homeURL?: string;
+  expandable?: boolean;
 }
 
-export default class OSSLayoutSidebar extends Component<OSSLayoutSidebarArgs> {}
+export default class OSSLayoutSidebar extends Component<OSSLayoutSidebarArgs> {
+  @tracked expanded: boolean = true;
+
+  upfLocalStorage = new UPFLocalStorage();
+
+  constructor(owner: unknown, args: OSSLayoutSidebarArgs) {
+    super(owner, args);
+
+    scheduleOnce('afterRender', this, this.initializeSidebarState);
+  }
+
+  get computedClasses(): string {
+    const classes = ['oss-sidebar-container', 'oss-sidebar--containers'];
+
+    if (this.expanded) {
+      classes.push('oss-sidebar-container--expanded');
+    }
+
+    return classes.join(' ');
+  }
+
+  @action
+  toggleExpandedState(): void {
+    this.expanded = !this.expanded;
+    this.upfLocalStorage.saveItem(SIDEBAR_EXPANDED_STATE, String(this.expanded));
+  }
+
+  private initializeSidebarState(): void {
+    this.expanded = Boolean(this.args.expandable) && this.upfLocalStorage.getItem(SIDEBAR_EXPANDED_STATE) !== 'false';
+  }
+}
