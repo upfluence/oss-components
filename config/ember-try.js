980b4c5a800cbe1ddaf5436c7814661cb7b6638e
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2025-10-20 14:53:55 +0200

    ember-try: test against ember 4.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,84 +1,27 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
-
 module.exports = async function () {
   return {
+    packageManager: 'pnpm',
+    command: 'ember test --silent -r dot',
     scenarios: [
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3'
-          }
-        }
-      },
       {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0'
+            'ember-source': '~3.28.8'
           }
         }
       },
       {
-        name: 'ember-release',
+        name: 'ember-4.12',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release')
+            'ember-source': '~4.12.3',
+            'ember-cli': '~4.12.3'
           }
         }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': false
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0'
-          }
-        }
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false
-          })
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0'
-          },
-          ember: {
-            edition: 'classic'
-          }
-        }
-      },
-      embroiderSafe(),
-      embroiderOptimized()
+      }
     ]
   };
 };
