7c52f1f2795b7bbd4b134cd1a2f762a7c5ec071f
Julien Vannier <julien.vannier66@gmail.com>
2025-12-01 15:14:43 +0100

    Add OSS::OnboardingState component

---
+++ addon/components/o-s-s/onboarding-state.ts
@@ -0,0 +1,18 @@
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+interface OSSOnboardingStateComponentSignature {
+  title: string;
+  subtitle: string;
+  imageUrl: string;
+}
+
+export default class OSSOnboardingStateComponent extends Component<OSSOnboardingStateComponentSignature> {
+  constructor(owner: unknown, args: OSSOnboardingStateComponentSignature) {
+    super(owner, args);
+
+    assert('[OSS::OnboardingState] The @title parameter is mandatory', typeof args.title === 'string');
+    assert('[OSS::OnboardingState] The @subtitle parameter is mandatory', typeof args.subtitle === 'string');
+    assert('[OSS::OnboardingState] The @imageUrl parameter is mandatory', typeof args.imageUrl === 'string');
+  }
+}
