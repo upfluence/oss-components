7aae5f300b05282c22becd85523d26ad83e73a69
Elodie Labbé <elodie.labbe@upfluence.com>
2026-01-19 15:30:15 +0100

    Cleanup anchor component, add doc and tests

--- tests/integration/components/o-s-s/anchor-test.ts
+++ tests/integration/components/o-s-s/anchor-test.ts
@@ -13,15 +13,29 @@ module('Integration | Component | o-s-s/anchor', function (hooks) {
     this.transitionToStub = sinon.stub(this.router, 'transitionTo');
   });
 
-  test('When link is registered in router it render as a anchor element', async function (assert) {
+  test('When link is not registered in router it renders as a anchor element', async function (assert) {
     await render(hbs`<OSS::Anchor @link="http://www.google.fr" target="_blank" >test</OSS::Anchor>`);
 
     assert.dom('a').hasNoClass('ember-view');
+    assert.dom('a').hasAttribute('href', 'http://www.google.fr');
   });
 
-  test('When link is registered in router it render as a linkTo helper', async function (assert) {
+  test('When engine is defined, it renders as a linkTo helper', async function (assert) {
+    const urlForStub = sinon.stub(this.router, 'urlFor').returns('/input');
+    await render(hbs`<OSS::Anchor @link="index" @engine="input" >test</OSS::Anchor>`);
+
+    assert.ok(urlForStub.calledWithExactly('input.index'));
+    assert.dom('a').hasClass('ember-view');
+    // '/' instead of '/input' because ember engines are doing the prefixing ('/input') internally
+    assert.dom('a').hasAttribute('href', '/');
+  });
+
+  test('When link is registered in router it renders as a linkTo helper', async function (assert) {
+    const urlForSpy = sinon.spy(this.router, 'urlFor');
     await render(hbs`<OSS::Anchor @link="index" >test</OSS::Anchor>`);
 
+    assert.ok(urlForSpy.calledWithExactly('index'));
     assert.dom('a').hasClass('ember-view');
+    assert.dom('a').hasAttribute('href', '/');
   });
 });
