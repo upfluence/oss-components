0c96beb8a7ba3c04c915103d86ff2fb79d176307
Edouard Misset <edouardmisset@gmail.com>
2026-03-12 15:55:44 +0100

    refactor: renaming to hasHtmlText

--- addon-test-support/register-assertions.ts
+++ addon-test-support/register-assertions.ts
@@ -1,8 +1,9 @@
 import tooltipAssertions from '@upfluence/oss-components/test-support/custom-assertions/tooltip';
 import infiniteSelectOptionAssertions from '@upfluence/oss-components/test-support/custom-assertions/infinite-select-option';
 import dcnAssertions from '@upfluence/oss-components/test-support/custom-assertions/dcn';
+import hasHtmlTextAssertions from '@upfluence/oss-components/test-support/custom-assertions/has-html-text';
 
-const ASSERTIONS = [tooltipAssertions, infiniteSelectOptionAssertions, dcnAssertions];
+const ASSERTIONS = [tooltipAssertions, infiniteSelectOptionAssertions, dcnAssertions, hasHtmlTextAssertions];
 
 export default function registerAssertions(assert: Assert) {
   ASSERTIONS.forEach((assertion) => {
