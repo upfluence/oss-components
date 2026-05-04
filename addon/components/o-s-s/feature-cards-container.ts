1d511ffb50b6101597d364c07abea4ceb3f96a9e
Maximilien B. <maximilien.brunelle@gmail.com>
2026-05-04 11:33:04 +0200

    Updated: FeatureCardsContainer - use oop methods - remove dead assertions

--- addon/components/o-s-s/feature-cards-container.ts
+++ addon/components/o-s-s/feature-cards-container.ts
@@ -7,26 +7,6 @@ type OSSFeatureCardsContainerArgs = {
   cards: OSSFeatureCardArgs[];
 };
 
-function isCenterCard(cardsCount: number, index: number): boolean {
-  return cardsCount === 1 || (cardsCount === 3 && index === 1);
-}
-
-function getDefaultCardColorVariant(cardsCount: number, index: number): OSSFeatureCardColorVariant {
-  if (isCenterCard(cardsCount, index)) return 'violet';
-
-  if (cardsCount === 2 || cardsCount === 3) return index === 0 ? 'blue' : 'yellow';
-
-  assert('[OSS::FeatureCardsContainer] Internal layout configuration mismatch', false);
-}
-
-function getDefaultCardShadowVariant(cardsCount: number, index: number): OSSFeatureCardShadowVariant {
-  if (isCenterCard(cardsCount, index)) return 'lg';
-
-  if (cardsCount === 2 || cardsCount === 3) return 'sm';
-
-  assert('[OSS::FeatureCardsContainer] Internal layout configuration mismatch', false);
-}
-
 export default class OSSFeatureCardsContainer extends Component<OSSFeatureCardsContainerArgs> {
   constructor(owner: unknown, args: OSSFeatureCardsContainerArgs) {
     super(owner, args);
@@ -43,8 +23,8 @@ export default class OSSFeatureCardsContainer extends Component<OSSFeatureCardsC
 
     return cards.map((card, index) => {
       const cardCount = cards.length;
-      const colorVariant = card.colorVariant ?? getDefaultCardColorVariant(cardCount, index);
-      const shadowVariant = card.shadowVariant ?? getDefaultCardShadowVariant(cardCount, index);
+      const colorVariant = card.colorVariant ?? this.getDefaultCardColorVariant(cardCount, index);
+      const shadowVariant = card.shadowVariant ?? this.getDefaultCardShadowVariant(cardCount, index);
 
       return {
         ...card,
@@ -52,5 +32,21 @@ export default class OSSFeatureCardsContainer extends Component<OSSFeatureCardsC
         shadowVariant
       };
     });
+  }
+
+  private getDefaultCardShadowVariant(cardsCount: number, index: number): OSSFeatureCardShadowVariant {
+    if (this.isCenterCard(cardsCount, index)) return 'lg';
+
+    return 'sm';
+  }
+
+  private getDefaultCardColorVariant(cardsCount: number, index: number): OSSFeatureCardColorVariant {
+    if (this.isCenterCard(cardsCount, index)) return 'violet';
+
+    return index === 0 ? 'blue' : 'yellow';
+  }
+
+  private isCenterCard(cardsCount: number, index: number): boolean {
+    return cardsCount === 1 || (cardsCount === 3 && index === 1);
   }
 }
