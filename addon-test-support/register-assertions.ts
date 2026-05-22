50f2a46bfe65d9824f7d0fafee94fa3ee746f936
Edouard Misset <edouardmisset@gmail.com>
2026-05-22 12:18:26 +0200

    fix: remove custom hasHtmlText assertion in favor of qunit-dom hasHtml

--- addon-test-support/register-assertions.ts
+++ addon-test-support/register-assertions.ts
@@ -1,9 +1,8 @@
 import tooltipAssertions from '@upfluence/oss-components/test-support/custom-assertions/tooltip';
 import infiniteSelectOptionAssertions from '@upfluence/oss-components/test-support/custom-assertions/infinite-select-option';
 import dcnAssertions from '@upfluence/oss-components/test-support/custom-assertions/dcn';
-import hasHtmlTextAssertions from '@upfluence/oss-components/test-support/custom-assertions/has-html-text';
 
-const ASSERTIONS = [tooltipAssertions, infiniteSelectOptionAssertions, dcnAssertions, hasHtmlTextAssertions];
+const ASSERTIONS = [tooltipAssertions, infiniteSelectOptionAssertions, dcnAssertions];
 
 export default function registerAssertions(assert: Assert) {
   ASSERTIONS.forEach((assertion) => {
