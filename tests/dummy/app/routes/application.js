ed09a7faeb562ca8bd515aca349cbf69dc32f70c
Owen Coogan <owencoogan01@gmail.com>
2026-05-22 15:20:17 +0200

    fix: fixed linting issue

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { IntlService } from 'ember-intl';
 
 export default class Application extends Route {
-  @service declare intl: IntlService;
+  @service intl;
 
   beforeModel() {
     this.intl.setLocale('en-us');
