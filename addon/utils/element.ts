7c39e1d2a7b7e8ff992671f9328dea98dcd8ac25
Olympe Lespagnon <olympe.lespagnon@upfluence.com>
2026-05-28 11:15:50 +0200

    Handle vertical overflow in enable-tooltip

--- addon/utils/element.ts
+++ addon/utils/element.ts
@@ -1,9 +1,16 @@
-export function hasOverflow(element: HTMLElement): boolean {
+export type OverflowDirection = 'horizontal' | 'vertical';
+
+export function hasOverflow(element: HTMLElement, direction: OverflowDirection = 'horizontal'): boolean {
   if (!Array.from(element.children).length) {
-    return element.offsetWidth < element.scrollWidth;
+    switch (direction) {
+      case 'horizontal':
+        return element.offsetWidth < element.scrollWidth;
+      case 'vertical':
+        return element.offsetHeight < element.scrollHeight;
+    }
   }
 
   return Array.from(element.children).some((child: Element) => {
-    return hasOverflow(child as HTMLElement);
+    return hasOverflow(child as HTMLElement, direction);
   });
 }
