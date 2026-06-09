ad936c50529afee0afc784f4361c9cbc8898c605
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2026-06-09 17:30:12 +0200

    Incorporate smart blob component from ember-upf-utils

---
+++ addon/components/o-s-s/smart/blob.ts
@@ -0,0 +1,34 @@
+import Component from '@glimmer/component';
+
+type Size = 'sm' | 'md' | 'xl';
+
+interface SmartBlobArgs {
+  loading?: boolean;
+  size?: Size;
+}
+
+export default class SmartBlob extends Component<SmartBlobArgs> {
+  get size(): Size {
+    return this.args.size ?? 'md';
+  }
+
+  get loadingClass(): string {
+    return this.args.loading ? 'fade-in' : 'fade-out';
+  }
+
+  get inactiveClass(): string {
+    return this.args.loading ? 'fade-out' : 'fade-in';
+  }
+
+  get computedClass(): string {
+    const classes = ['smart-blob-container'];
+
+    if (this.args.loading) {
+      classes.push('smart-blob-container--active');
+    }
+
+    classes.push(`smart-blob-container--${this.size}`);
+
+    return classes.join(' ');
+  }
+}
