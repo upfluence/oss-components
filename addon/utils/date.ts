48f136f3631cc5a8fdcc29376857087e0023e3a9
Maximilien B <maximilien.brunelle@gmail.com>
2026-08-12 09:25:16 +0200

    Added default date format and renamed the other

--- addon/utils/date.ts
+++ addon/utils/date.ts
@@ -1,1 +1,2 @@
-export const DEFAULT_DATE_FORMAT = 'MMM DD, YYYY - hh:mma';
+export const DEFAULT_DATE_FORMAT = 'MMM DD, YYYY';
+export const DEFAULT_DATE_FORMAT_WITH_TIME = 'MMM DD, YYYY - hh:mma';
